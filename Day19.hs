1ffa53c0c1bbc8619b28ce455441ca1768c8d530
Matt Chowning <mchowning@gmail.com>
2015-12-20 12:02:55 -0500

    day19, part 2

--- Day19.hs
+++ Day19.hs
@@ -7,13 +7,16 @@ import Data.Maybe
 import Data.List(nub)
 import Data.List.Split(splitOn)
 import Data.Text(Text)
-import qualified Data.Text as T
 import Test.HUnit
 import Text.Printf
+
+import qualified Data.Char as C
+import qualified Data.Text as T
 
 results :: IO ()
 results = do input <- readFile "day19_input.txt"
              printResult 1 (result1 input)
+             printResult 2 (result2 input)
   where
     printResult :: Int -> Int -> IO ()
     printResult = printf "result %d: %d\n"
@@ -22,6 +25,42 @@ results = do input <- readFile "day19_input.txt"
 -- 509  
 result1 :: String -> Int
 result1 = length . nub . uncurry allPossibleSubs . parseInput
+
+{- The rules have formats of these types:
+  _
+  Rn_Ar
+  Rn_Y_Ar
+  Rn_Y_Y_Ar
+In essence, treat the 'Rn' and 'Ar' as parentheses, the 'Y' and the Y as a comma.
+Also, all mapping inputs are a single capitalized letter or a capitalized letter
+followed by a lowercase letter. 
+ 
+So you can count the number of elements in the end state by counting the number 
+of capitalized letters.  Use this number as the starting point because it 
+is the maximum number of transitions you would need to get to the conclusion. 
+Subtract 1 from that number because you start with 'e' (which transitions into 2 
+elements) for free.  Since 'Rn' and 'Ar' and 'Y' are always additional 'elements' 
+you  get from a transition for free, subtract the number of those in the conclusion 
+from the previously calculated max possible number of transitions.  Likewise, every 
+'Y' element has another as-yet-uncounted element after it (i.e., 'Y' is never at 
+the end of a mapping or immediately followed by an 'Rn', 'Ar', or another 'Y'), so 
+subtract the number of 'Y' elements a second time.  This gives you the only number of
+transitions that can achieve a a transition from 'e' (or any other single starting 
+element) to the relevant end state.
+
+Result was 195 for my input. -}
+result2 :: String -> Int
+result2 str = let molecule = fst . parseInput $ str
+                  numSymbols = length . filter C.isUpper . T.unpack $ molecule
+                  numParens = numOccurrences "Rn" molecule + numOccurrences "Ar" molecule
+                  numComma = numOccurrences "Y" molecule
+                  numElementsFollowingComma = numComma
+                  numStartingElements = 1
+              in numSymbols - numParens - numComma - numElementsFollowingComma - numStartingElements
+
+numOccurrences :: Text -> Text -> Int
+numOccurrences matcher = length . filter (T.isPrefixOf matcher) . T.tails
+
 
 parseInput :: String -> (Text, [(Text,Text)])
 parseInput str = let [unparsedMappings,molecule] = splitOn [""] . T.lines . T.pack $ str
