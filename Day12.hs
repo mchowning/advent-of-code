579c720635bb01058abe1e4f0bdb9dd86e98ab50
Matt Chowning <mchowning@gmail.com>
2016-01-07 08:10:15 -0500

    day12 part2

--- Day12.hs
+++ Day12.hs
@@ -1,37 +1,73 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Day12 where
 
-import Control.Monad(liftM2)
 import Data.Char(isDigit)
+import Data.Function(on)
 import Data.List(groupBy)
 import Data.Maybe
 import Test.HUnit
-import Text.Printf(printf)
 import Text.Read(readMaybe)
+import Text.Printf(printf)
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.HashMap.Strict as H
+import qualified Data.Scientific as S
+import qualified Data.Vector as V
+
+import Data.Aeson
 
 results :: IO ()
-results = do input <- readFile "day12_input.txt"
-             printResult 1 (result1 input)
+results = do input <- B.readFile "day12_input.txt"
+             printResult 1 (result1 $ show input)
+             printResult 2 (result2 input)
   where
     printResult :: Int -> Int -> IO ()
     printResult = printf "result %d: %d\n"
 
+--- Part 1
+
+-- 119433
 result1 :: String -> Int
 result1 = sum . mapMaybe readMaybe . groupBy areSameCharType
 
-result2 :: String -> Int
-result2 = undefined
-
-isNum :: String -> Bool
-isNum str = isJust (readMaybe str :: Maybe Int)
-
--- TODO find better way to do this
 areSameCharType :: Char -> Char -> Bool
-areSameCharType c1 c2 = isNumRelatedChar c1 == isNumRelatedChar c2 
+areSameCharType = (==) `on` isNumRelatedChar
 
 isNumRelatedChar :: Char -> Bool
-isNumRelatedChar = liftM2 (||) isDigit (== '-')
+isNumRelatedChar = (||) <$> isDigit <*> (== '-')
+
+--- Part 2
+
+-- 68466
+result2 :: B.ByteString -> Int
+result2 = valueOfObject . decodeJson
+
+decodeJson :: B.ByteString -> Object
+decodeJson = fromMaybe parseError . decode
+  where
+    parseError = error "failed to parse json"
+
+valueOfValue :: Value -> Int
+valueOfValue (Number s) = fromJust $ S.toBoundedInteger s
+valueOfValue (Array vector)  = V.sum . V.map valueOfValue $ vector
+valueOfValue (Object o) = valueOfObject o
+valueOfValue _          = 0
+
+valueOfObject :: Object -> Int
+valueOfObject o = if isObjectAllowed o then
+                    sum . map valueOfValue . H.elems $ o
+                  else
+                    0
+  where
+    isObjectAllowed :: Object -> Bool
+    isObjectAllowed = all isValueAllowed . H.elems
+
+    isValueAllowed :: Value -> Bool
+    isValueAllowed (String "red") = False
+    isValueAllowed _              = True
+
 
 -------------------------------------------------------------------------------
 -- TESTS ----------------------------------------------------------------------
@@ -39,25 +75,45 @@ isNumRelatedChar = liftM2 (||) isDigit (== '-')
 
 main :: IO Counts
 main = runTestTT $ TestList 
-  [ isNumRelatedChar 'a' ~?= False
-  , isNumRelatedChar '1' ~?= True
-  , isNumRelatedChar '-' ~?= True
+  [ TestLabel "isNumRelatedChar" $ TestList
+    [ isNumRelatedChar 'a' ~?= False
+    , isNumRelatedChar '1' ~?= True
+    , isNumRelatedChar '-' ~?= True
+    ]
 
-  , isNum "12" ~?= True
-  , isNum "-5" ~?= True
-  , isNum "0" ~?= True
-  , isNum "a" ~?= False
+  , TestLabel "result1" $ TestList
+    [ result1 "[1,2,3]" ~?= 6
+    , result1 "[[[3]]]" ~?= 3
+    , result1 "{\"a\":{\"b\":4},\"c\":-1}" ~?= 3
+    , result1 "{\"a\":[-1,1]}" ~?= 0
+    , result1 "[-1,{\"a\":1}]" ~?= 0
+    , result1 "[]" ~?= 0
+    , result1 "{}" ~?= 0
+    ]
 
-  , result1 "[1,2,3]" ~?= 6
-  , result1 "[[[3]]]" ~?= 3
-  , result1 "{\"a\":{\"b\":4},\"c\":-1}" ~?= 3
-  , result1 "{\"a\":[-1,1]}" ~?= 0
-  , result1 "[-1,{\"a\":1}]" ~?= 0
-  , result1 "[]" ~?= 0
-  , result1 "{}" ~?= 0
-
-  --, result2 "[1,2,3]" ~?= 6
-  --, result2 "[1,{\"c\":\"red\",\"b\":2},3]" ~?= 4
-  --, result2 "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}" ~?= 0
-  --, result2 "[1,\"red\",5]" ~?= 6
+  , TestLabel "result2" $ TestList
+    [ result2 sampleObject ~?= 2
+    , result2 sampleArray ~?= 12
+    , result2 sampleObjectWithRed ~?= 0
+    , result2 sampleObjectWithoutRed ~?= 390
+    , result2 sampleNestedObject ~?= 12
+    , result2 "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}" ~?= 0
+    , result2 "{\"d\":\"blue\",\"e\":[1,2,3,4],\"f\":5}" ~?= 15
+    ]
   ]
+    where
+      sampleObject :: B.ByteString
+      sampleObject = "{\"name\":\"Dave\",\"age\":2}"
+
+      sampleArray :: B.ByteString
+      sampleArray = "{\"numbers\":[3,4,5]}"
+
+      sampleObjectWithRed :: B.ByteString
+      sampleObjectWithRed = "{\"objectThatMustNotBeNamed\":\"red\",\"arrayWithALotOfNumbers\":[12,324,54]}"
+
+
+      sampleObjectWithoutRed :: B.ByteString
+      sampleObjectWithoutRed = "{\"acceptableObject\":\"blue\",\"arrayWithALotOfNumbers\":[12,324,54]}"
+
+      sampleNestedObject :: B.ByteString
+      sampleNestedObject = "{\"name\":\"Dave\",\"anotherObject\":{\"numbers\":[3,4,5]}}"
