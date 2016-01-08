5f3712ba7ca89ba16740076ef26427483f6c6199
Matt Chowning <mchowning@gmail.com>
2016-01-08 13:04:48 -0500

    day7, progress but still have work

--- Day7.hs
+++ Day7.hs
@@ -2,6 +2,7 @@
 
 module Day7 where
 
+import Control.Applicative
 import Data.Bits
 import Data.List
 import Data.List.Split
@@ -21,17 +22,74 @@ results = do input <- readFile "day7_input.txt"
     printResult :: Int -> String -> IO ()
     printResult = printf "result %d: %s\n"
 
+--- part 1
+
 getValue :: Identifier -> String -> String
 getValue ident = getValueFromMap ident . valueMapper
-
-getValueWithStartValues :: Identifier -> String -> [(Identifier,Word16)] -> String
-getValueWithStartValues ident input presets = getValueFromMap ident . valueMapperWithStartValues input $ presets
 
 getValueFromMap :: Identifier -> [(Identifier,Word16)] -> String
 getValueFromMap ident = show . snd . head . filter ((== ident) . fst)
 
 valueMapper :: String -> [(Identifier,Word16)]
 valueMapper str = mapValues ([],lines str)
+
+mapValues :: ([(Identifier,Word16)],[String]) -> [(Identifier,Word16)]
+mapValues (result,[]) = result
+mapValues (result,x:xs) = let mappedValue = mapValue x
+                          in if isNothing mappedValue then
+                               mapValues (result, xs ++ [x])
+                             else
+                               let newValue = fromJust mappedValue
+                                   newInputs = updateInputs newValue xs
+                               in mapValues (newValue:result, newInputs)
+
+-- TODO good place to use Maybe as Monad
+mapValue :: String -> Maybe (Identifier,Word16)
+mapValue str1 = let strWords = words str1
+                    identifier = last strWords
+                    value = parseInput (unwords . init . init $ strWords)
+                in if isNothing value then Nothing else Just (identifier, fromJust value)
+
+-- (>>=) :: Maybe m => m a -> (a -> m b) -> m b
+-- ()    :: Maybe m => m a -> m b -> (a -> b -> m c) -> m c
+
+
+-- TODO use monad to apply all these "parsers" and keep the result of the one that succeeds (library?)
+-- TODO probably a good chance to use a Parser Monad
+parseInput :: String -> Maybe Word16
+parseInput str2
+  | isJust (readMaybe str2::Maybe Word16) = readMaybe str2
+--   | "AND" `isInfixOf` str2 = (\[x,y] -> (.&.) <$> readMaybe x <*> readMaybe y) . splitOn " AND " $ str2
+  | "AND" `isInfixOf` str2 =  foldr1 (liftA2 (.&.)) . map readMaybe . splitOn " AND " $ str2
+--   | "OR" `isInfixOf` str2 = (\[x,y] -> (.|.) <$> readMaybe x <*> readMaybe y) . splitOn " OR " $ str2
+  | "OR" `isInfixOf` str2 = foldr1 (liftA2 (.|.)) . map readMaybe . splitOn " OR " $ str2
+  | "LSHIFT" `isInfixOf` str2 = lShift . splitOn " LSHIFT " $ str2
+  | "RSHIFT" `isInfixOf` str2 = rShift . splitOn " RSHIFT " $ str2
+  | "NOT" `isInfixOf` str2 = complement <$> (readMaybe . last $ words str2)
+  | otherwise = Nothing
+    where
+      lShift :: [String] -> Maybe Word16
+      lShift [w,i] = shift <$> readMaybe w <*> readMaybe i
+      lShift _     = undefined
+
+      rShift :: [String] -> Maybe Word16
+      rShift [w,i] = shift <$> readMaybe w <*> (negate <$> readMaybe i)
+      rShift _     = undefined
+
+updateInputs :: (Identifier,Word16) -> [String] -> [String]
+updateInputs (id1,val) = map (replace id1 val)
+  where
+    replace :: Identifier -> Word16 -> String -> String
+    --replace identifier w = map (\x -> if (x /= identifier) then x else show w) . words
+    replace identifier w line = unwords . map (replaceIfMatch identifier w) . words $ line
+
+    replaceIfMatch :: Identifier -> Word16 -> String -> String
+    replaceIfMatch ident newVal input = if input /= ident then input else show newVal
+
+--- part 2
+
+getValueWithStartValues :: Identifier -> String -> [(Identifier,Word16)] -> String
+getValueWithStartValues ident input presets = getValueFromMap ident . valueMapperWithStartValues input $ presets
 
 valueMapperWithStartValues :: String -> [(Identifier,Word16)] -> [(Identifier,Word16)]
 valueMapperWithStartValues str startVals = 
@@ -47,57 +105,6 @@ filterInputs presets = filter (flip notAlreadySet presets . last . splitOn " -> 
 notAlreadySet :: Identifier -> [(Identifier,Word16)] -> Bool
 notAlreadySet ident = not . any ((== ident) . fst)
 
-mapValues :: ([(Identifier,Word16)],[String]) -> [(Identifier,Word16)]
-mapValues (result,[]) = result
-mapValues (result,x:xs) = let mappedValue = mapValue x
-                          in if isNothing mappedValue
-                             then mapValues (result, xs ++ [x])
-                             else let newValue = fromJust mappedValue
-                                      newInputs = updateInputs newValue xs
-                                  in mapValues (newValue:result, newInputs)
-
-mapValue :: String -> Maybe (Identifier,Word16)
-mapValue str1 = let strWords = words str1
-                    identifier = last strWords
-                    value = parseInput (unwords . init . init $ strWords)
-                in if isNothing value then Nothing else Just (identifier, fromJust value)
-
-parseInput :: String -> Maybe Word16
-parseInput str2 
-  | isJust (readMaybe str2::Maybe Word16) = readMaybe str2
-  | "AND" `isInfixOf` str2 = (\[x,y] -> parseAnd (readMaybe x) (readMaybe y)) . splitOn " AND " $ str2
-  | "OR" `isInfixOf` str2 = (\[x,y] -> parseOr (readMaybe x) (readMaybe y)) . splitOn " OR " $ str2
-  | "LSHIFT" `isInfixOf` str2 = (\[x,y] -> parseShift (readMaybe x) (read y)) . splitOn " LSHIFT " $ str2
-  | "RSHIFT" `isInfixOf` str2 = (\[x,y] -> parseShift (readMaybe x) (negate . read $ y)) . splitOn " RSHIFT " $ str2
-  | "NOT" `isInfixOf` str2 = parseNot (readMaybe . last . words $ str2)
-  | otherwise = Nothing
-
-parseAnd :: Maybe Word16 -> Maybe Word16 -> Maybe Word16
-parseAnd (Just x) (Just y) = Just (x .&. y)
-parseAnd _ _ = Nothing
-
-parseOr :: Maybe Word16 -> Maybe Word16 -> Maybe Word16
-parseOr (Just x) (Just y) = Just (x .|. y)
-parseOr _ _ = Nothing
-
-parseShift :: Maybe Word16 -> Int -> Maybe Word16
-parseShift Nothing _ = Nothing
-parseShift (Just v) n = Just (shift v n)
-
--- TODO good simple function to use for learning monads
-parseNot :: Maybe Word16 -> Maybe Word16
-parseNot Nothing = Nothing
-parseNot (Just v) = Just (complement v)
-
-updateInputs :: (Identifier,Word16) -> [String] -> [String]
-updateInputs (id1,val) = map (replace id1 val)
-  where 
-    replace :: Identifier -> Word16 -> String -> String
-    --replace identifier w = map (\x -> if (x /= identifier) then x else show w) . words 
-    replace identifier w line = unwords . map (replaceIfMatch identifier w) . words $ line
-    
-    replaceIfMatch :: Identifier -> Word16 -> String -> String
-    replaceIfMatch ident newVal input = if input /= ident then input else show newVal
 
 -------------------------------------------------------------------------------
 -- TESTS ----------------------------------------------------------------------
