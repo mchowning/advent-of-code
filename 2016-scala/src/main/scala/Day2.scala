1897cb0adb77225a3959b97abfef83083177a996
Matt Chowning <mchowning@gmail.com>
2016-12-06 17:02:05 -0500

    handle edge moves

--- 2016-scala/src/main/scala/Day2.scala
+++ 2016-scala/src/main/scala/Day2.scala
@@ -1,15 +1,21 @@
 
 object Day2 {
 
+  val keypadSize = 3
+
   type Coordinate = (Int, Int)
 
   def move(coordinate: Coordinate, direction: Char): Coordinate = {
+
+    def maxLimit(i: Int): Int = Math.min(i, keypadSize - 1)
+    def minLimit(i: Int): Int = Math.max(i, 0)
+
     val (x,y) = coordinate
     direction match {
-      case 'U' => (x    , y + 1)
-      case 'R' => (x + 1, y    )
-      case 'D' => (x    , y - 1)
-      case 'L' => (x - 1, y    )
+      case 'U' => (x              , maxLimit(y + 1))
+      case 'R' => (maxLimit(x + 1), y              )
+      case 'D' => (x              , minLimit(y - 1))
+      case 'L' => (minLimit(x - 1), y              )
     }
   }
 }
--- 2016-scala/src/test/scala/Day2Spec.scala
+++ 2016-scala/src/test/scala/Day2Spec.scala
@@ -3,11 +3,27 @@ import org.scalatest.FreeSpec
 class Day2Spec extends FreeSpec {
 
   "Day2" - {
-     "handles non edge moves" in {
-       assert(Day2.move((1,1), 'U') == (1,2))
-       assert(Day2.move((1,1), 'R') == (2,1))
-       assert(Day2.move((1,1), 'D') == (1,0))
-       assert(Day2.move((1,1), 'L') == (0,1))
-     }
+    "handles non edge moves" in {
+      assert(Day2.move((1,1), 'U') == (1,2))
+      assert(Day2.move((1,1), 'R') == (2,1))
+      assert(Day2.move((1,1), 'D') == (1,0))
+      assert(Day2.move((1,1), 'L') == (0,1))
+    }
+    "handles edge moves" in {
+      assert(Day2.move((0,0), 'L') == (0,0))
+      assert(Day2.move((0,0), 'D') == (0,0))
+      assert(Day2.move((1,0), 'D') == (1,0))
+      assert(Day2.move((2,0), 'D') == (2,0))
+      assert(Day2.move((2,0), 'R') == (2,0))
+
+      assert(Day2.move((0,1), 'L') == (0,1))
+      assert(Day2.move((2,1), 'R') == (2,1))
+
+      assert(Day2.move((0,2), 'L') == (0,2))
+      assert(Day2.move((0,2), 'U') == (0,2))
+      assert(Day2.move((1,2), 'U') == (1,2))
+      assert(Day2.move((2,2), 'U') == (2,2))
+      assert(Day2.move((2,2), 'R') == (2,2))
+    }
   }
 }
