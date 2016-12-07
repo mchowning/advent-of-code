11790cb3c05fa916f18b578d479a157eda5d9525
Matt Chowning <mchowning@gmail.com>
2016-12-07 09:52:13 -0500

    Use matchers

--- 2016-scala/src/test/scala/Day1Spec.scala
+++ 2016-scala/src/test/scala/Day1Spec.scala
@@ -1,79 +1,80 @@
 import java.io.InputStream
 
 import Day1._
-import org.scalatest.FreeSpec
+import org.scalatest.{Matchers, FreeSpec}
 
-class Day1Spec extends FreeSpec {
+class Day1Spec extends FreeSpec with Matchers {
 
   "Day1" - {
     "can parse moves" in {
-      assert(Day1.parseMoves("R1, L3, R2") == List((RightTurn, 1), (LeftTurn, 3), (RightTurn, 2)))
+      Day1.parseMoves("R1, L3, R2") shouldBe List((RightTurn, 1), (LeftTurn, 3), (RightTurn, 2))
     }
     "can update a direction based on a turn" in {
-      assert(Day1.updateDirection(North, RightTurn) == East)
-      assert(Day1.updateDirection(North, LeftTurn) == West)
-      assert(Day1.updateDirection(East, RightTurn) == South)
-      assert(Day1.updateDirection(East, LeftTurn) == North)
-      assert(Day1.updateDirection(South, RightTurn) == West)
-      assert(Day1.updateDirection(South, LeftTurn) == East)
-      assert(Day1.updateDirection(West, RightTurn) == North)
-      assert(Day1.updateDirection(West, LeftTurn) == South)
+      Day1.updateDirection(North, RightTurn) shouldBe East
+      Day1.updateDirection(North, LeftTurn)  shouldBe West
+      Day1.updateDirection(East, RightTurn)  shouldBe South
+      Day1.updateDirection(East, LeftTurn)   shouldBe North
+      Day1.updateDirection(South, RightTurn) shouldBe West
+      Day1.updateDirection(South, LeftTurn)  shouldBe East
+      Day1.updateDirection(West, RightTurn)  shouldBe North
+      Day1.updateDirection(West, LeftTurn)   shouldBe South
     }
     "can update the direction of the initial position based on" - {
       def getDirection(pos: Position): Direction = pos match {case (p, _) => p}
       "a move with a right turn" in {
         val moves: List[(Turn, Int)] = Day1.parseMoves("R1")
-        assert(getDirection(Day1.getFinalPosition(moves)) == East)
+        getDirection(Day1.getFinalPosition(moves)) shouldBe East
       }
       "a move with a left turn" in {
         val moves: List[(Turn, Int)] = Day1.parseMoves("L1")
-        assert(getDirection(Day1.getFinalPosition(moves)) == West)
+        getDirection(Day1.getFinalPosition(moves)) shouldBe West
       }
       "a list of moves" in {
         val moves: List[(Turn, Int)] = Day1.parseMoves("L1, R1, R1, R1")
-        assert(getDirection(Day1.getFinalPosition(moves)) == South)
+        getDirection(Day1.getFinalPosition(moves)) shouldBe South
       }
     }
     "can determine the final position from a list of moves" in {
+
       val moves1: List[(Turn, Int)] = Day1.parseMoves("R2, L3")
-      assert(Day1.getFinalPosition(moves1) ==(North, (2, 3)))
+      Day1.getFinalPosition(moves1) shouldBe (North, (2, 3))
+
       val moves2: List[(Turn, Int)] = Day1.parseMoves("R2, R2, R2")
-      assert(Day1.getFinalPosition(moves2) ==(West, (0, -2)))
+      Day1.getFinalPosition(moves2) shouldBe (West, (0, -2))
+
       val moves: List[(Turn, Int)] = Day1.parseMoves("R5, L5, R5, R3")
-      assert(Day1.getFinalPosition(moves) ==(South, (10, 2)))
+      Day1.getFinalPosition(moves) shouldBe (South, (10, 2))
     }
     "can determine the distance of the final position from a list of moves" in {
-      assert(Day1.getFinalPositionDistance("R2, L3") == 5)
-      assert(Day1.getFinalPositionDistance("R2, R2, R2") == 2)
-      assert(Day1.getFinalPositionDistance("R5, L5, R5, R3") == 12)
+      Day1.getFinalPositionDistance("R2, L3") shouldBe 5
+      Day1.getFinalPositionDistance("R2, R2, R2") shouldBe 2
+      Day1.getFinalPositionDistance("R5, L5, R5, R3") shouldBe 12
     }
     "can solve part 1 of the problem" in {
-      val stream: InputStream = getClass.getResourceAsStream("input_day1")
+      val stream: InputStream = getClass.getResourceAsStream("input_day1.txt")
       val input = io.Source.fromInputStream(stream).mkString
-      assert(Day1.getFinalPositionDistance(input) == 307)
+      Day1.getFinalPositionDistance(input) shouldBe 307
     }
     "with a starting position determine all positions visited during a series of moves" in {
-       assert(Day1.getVisitedPositions(Day1.parseMoves("R2, L1")) ==
-              List((East, (1, 0)),
-                   (East, (2, 0)),
-                   (North, (2, 1))))
-       assert(Day1.getVisitedPositions(Day1.parseMoves("L2, L1")) ==
-              List((West, (-1, 0)),
-                   (West, (-2, 0)),
-                   (South, (-2, -1))))
+       Day1.getVisitedPositions(Day1.parseMoves("R2, L1")) shouldBe  List((East, (1, 0)),
+                                                                          (East, (2, 0)),
+                                                                          (North, (2, 1)))
+       Day1.getVisitedPositions(Day1.parseMoves("L2, L1")) shouldBe List((West, (-1, 0)),
+                                                                         (West, (-2, 0)),
+                                                                         (South, (-2, -1)))
     }
     "can determine the first position visited twice in a series of moves" in {
       val firstRepeatPosition = Day1.getFirstRepeatCoordinates(Day1.parseMoves("R8, R4, R4, R8"))
-      assert(firstRepeatPosition == (4, 0))
+      firstRepeatPosition shouldBe (4, 0)
     }
     "can determine the distance to the first position visited twice in a series of moves" in {
       val distanceToFirstRepeatPosition = Day1.getDistanceToFirstRepeatCoordinates("R8, R4, R4, R8")
-      assert(distanceToFirstRepeatPosition == 4)
+      distanceToFirstRepeatPosition shouldBe 4
     }
     "can solve part 2" in {
-      val stream: InputStream = getClass.getResourceAsStream("input_day1")
+      val stream: InputStream = getClass.getResourceAsStream("input_day1.txt")
       val input = io.Source.fromInputStream(stream).mkString
-      assert(getDistanceToFirstRepeatCoordinates(input) == 165)
+      getDistanceToFirstRepeatCoordinates(input) shouldBe 165
     }
   }
 }
--- 2016-scala/src/test/scala/Day2Spec.scala
+++ 2016-scala/src/test/scala/Day2Spec.scala
@@ -1,160 +1,160 @@
 import java.io.InputStream
 
-import org.scalatest.FreeSpec
+import org.scalatest.{Matchers, FreeSpec}
 import util.PipeOps._
 
-class Day2Spec extends FreeSpec {
+class Day2Spec extends FreeSpec with Matchers {
 
   "Day2" - {
     "handles non edge moves" in {
-      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'U') ==(1,0))
-      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'R') ==(2,1))
-      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'D') ==(1,2))
-      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'L') ==(0,1))
+      Day2.getPart1CoordinateFromSingleMove((1,1), 'U') shouldBe (1,0)
+      Day2.getPart1CoordinateFromSingleMove((1,1), 'R') shouldBe (2,1)
+      Day2.getPart1CoordinateFromSingleMove((1,1), 'D') shouldBe (1,2)
+      Day2.getPart1CoordinateFromSingleMove((1,1), 'L') shouldBe (0,1)
     }
     "handles edge moves" in {
-      assert(Day2.getPart1CoordinateFromSingleMove((0,0), 'L') ==(0,0))
-      assert(Day2.getPart1CoordinateFromSingleMove((0,0), 'U') ==(0,0))
-      assert(Day2.getPart1CoordinateFromSingleMove((1,0), 'U') ==(1,0))
-      assert(Day2.getPart1CoordinateFromSingleMove((2,0), 'U') ==(2,0))
-      assert(Day2.getPart1CoordinateFromSingleMove((2,0), 'R') ==(2,0))
+      Day2.getPart1CoordinateFromSingleMove((0,0), 'L') shouldBe (0,0)
+      Day2.getPart1CoordinateFromSingleMove((0,0), 'U') shouldBe (0,0)
+      Day2.getPart1CoordinateFromSingleMove((1,0), 'U') shouldBe (1,0)
+      Day2.getPart1CoordinateFromSingleMove((2,0), 'U') shouldBe (2,0)
+      Day2.getPart1CoordinateFromSingleMove((2,0), 'R') shouldBe (2,0)
 
-      assert(Day2.getPart1CoordinateFromSingleMove((0,1), 'L') ==(0,1))
-      assert(Day2.getPart1CoordinateFromSingleMove((2,1), 'R') ==(2,1))
+      Day2.getPart1CoordinateFromSingleMove((0,1), 'L') shouldBe (0,1)
+      Day2.getPart1CoordinateFromSingleMove((2,1), 'R') shouldBe (2,1)
 
-      assert(Day2.getPart1CoordinateFromSingleMove((0,2), 'L') ==(0,2))
-      assert(Day2.getPart1CoordinateFromSingleMove((0,2), 'D') ==(0,2))
-      assert(Day2.getPart1CoordinateFromSingleMove((1,2), 'D') ==(1,2))
-      assert(Day2.getPart1CoordinateFromSingleMove((2,2), 'D') ==(2,2))
-      assert(Day2.getPart1CoordinateFromSingleMove((2,2), 'R') ==(2,2))
+      Day2.getPart1CoordinateFromSingleMove((0,2), 'L') shouldBe (0,2)
+      Day2.getPart1CoordinateFromSingleMove((0,2), 'D') shouldBe (0,2)
+      Day2.getPart1CoordinateFromSingleMove((1,2), 'D') shouldBe (1,2)
+      Day2.getPart1CoordinateFromSingleMove((2,2), 'D') shouldBe (2,2)
+      Day2.getPart1CoordinateFromSingleMove((2,2), 'R') shouldBe (2,2)
     }
     "handles multiple moves" in {
-      assert(Day2.getPart1CoordinateFromMultipleMoves((1,1), "ULL") ==(0,0))
-      assert(Day2.getPart1CoordinateFromMultipleMoves((0,0), "RRDDD") ==(2,2))
-      assert(Day2.getPart1CoordinateFromMultipleMoves((2,2), "LURDL") ==(1,2))
-      assert(Day2.getPart1CoordinateFromMultipleMoves((1,2), "UUUUD") ==(1,1))
+      Day2.getPart1CoordinateFromMultipleMoves((1,1), "ULL") shouldBe (0,0)
+      Day2.getPart1CoordinateFromMultipleMoves((0,0), "RRDDD") shouldBe (2,2)
+      Day2.getPart1CoordinateFromMultipleMoves((2,2), "LURDL") shouldBe (1,2)
+      Day2.getPart1CoordinateFromMultipleMoves((1,2), "UUUUD") shouldBe (1,1)
     }
     "converts position to digit" in {
-      assert(Day2.getPart1Digit((0,0)) == 1)
-      assert(Day2.getPart1Digit((1,0)) == 2)
-      assert(Day2.getPart1Digit((2,0)) == 3)
-      assert(Day2.getPart1Digit((0,1)) == 4)
-      assert(Day2.getPart1Digit((1,1)) == 5)
-      assert(Day2.getPart1Digit((2,1)) == 6)
-      assert(Day2.getPart1Digit((0,2)) == 7)
-      assert(Day2.getPart1Digit((1,2)) == 8)
-      assert(Day2.getPart1Digit((2,2)) == 9)
+      Day2.getPart1Digit((0,0)) shouldBe 1
+      Day2.getPart1Digit((1,0)) shouldBe 2
+      Day2.getPart1Digit((2,0)) shouldBe 3
+      Day2.getPart1Digit((0,1)) shouldBe 4
+      Day2.getPart1Digit((1,1)) shouldBe 5
+      Day2.getPart1Digit((2,1)) shouldBe 6
+      Day2.getPart1Digit((0,2)) shouldBe 7
+      Day2.getPart1Digit((1,2)) shouldBe 8
+      Day2.getPart1Digit((2,2)) shouldBe 9
     }
     "determines valid coordinates for part 1" in {
-      assert(!Day2.isValidPart1Coord((0, -1)))
-      assert(!Day2.isValidPart1Coord((1, -1)))
-      assert(!Day2.isValidPart1Coord((2, -1)))
-      assert(!Day2.isValidPart1Coord((-1,0)))
-      assert(!Day2.isValidPart1Coord((3,0)))
-      assert(!Day2.isValidPart1Coord((-1,1)))
-      assert(!Day2.isValidPart1Coord((3,1)))
-      assert(!Day2.isValidPart1Coord((-1,2)))
-      assert(!Day2.isValidPart1Coord((3,2)))
-      assert(!Day2.isValidPart1Coord((0,3)))
-      assert(!Day2.isValidPart1Coord((1,3)))
-      assert(!Day2.isValidPart1Coord((2,3)))
+      Day2.isValidPart1Coord((0, -1)) shouldBe false
+      Day2.isValidPart1Coord((1, -1)) shouldBe false
+      Day2.isValidPart1Coord((2, -1)) shouldBe false
+      Day2.isValidPart1Coord((-1,0))  shouldBe false
+      Day2.isValidPart1Coord((3,0))   shouldBe false
+      Day2.isValidPart1Coord((-1,1))  shouldBe false
+      Day2.isValidPart1Coord((3,1))   shouldBe false
+      Day2.isValidPart1Coord((-1,2))  shouldBe false
+      Day2.isValidPart1Coord((3,2))   shouldBe false
+      Day2.isValidPart1Coord((0,3))   shouldBe false
+      Day2.isValidPart1Coord((1,3))   shouldBe false
+      Day2.isValidPart1Coord((2,3))   shouldBe false
 
-      assert(Day2.isValidPart1Coord((0,0)))
-      assert(Day2.isValidPart1Coord((1,0)))
-      assert(Day2.isValidPart1Coord((2,0)))
-      assert(Day2.isValidPart1Coord((0,1)))
-      assert(Day2.isValidPart1Coord((1,1)))
-      assert(Day2.isValidPart1Coord((2,1)))
-      assert(Day2.isValidPart1Coord((0,1)))
-      assert(Day2.isValidPart1Coord((1,1)))
-      assert(Day2.isValidPart1Coord((2,1)))
+      Day2.isValidPart1Coord((0,0)) shouldBe true
+      Day2.isValidPart1Coord((1,0)) shouldBe true
+      Day2.isValidPart1Coord((2,0)) shouldBe true
+      Day2.isValidPart1Coord((0,1)) shouldBe true
+      Day2.isValidPart1Coord((1,1)) shouldBe true
+      Day2.isValidPart1Coord((2,1)) shouldBe true
+      Day2.isValidPart1Coord((0,1)) shouldBe true
+      Day2.isValidPart1Coord((1,1)) shouldBe true
+      Day2.isValidPart1Coord((2,1)) shouldBe true
     }
     "determines valid coordinates for part 2" in {
 
-      assert(!Day2.isValidPart2Coord((0,0)))
-      assert(!Day2.isValidPart2Coord((1,0)))
-      assert(Day2.isValidPart2Coord((2,0)))
-      assert(!Day2.isValidPart2Coord((3,0)))
-      assert(!Day2.isValidPart2Coord((4,0)))
-      assert(!Day2.isValidPart2Coord((2, -1)))
+      Day2.isValidPart2Coord((0,0))   shouldBe false
+      Day2.isValidPart2Coord((1,0))   shouldBe false
+      Day2.isValidPart2Coord((2,0))   shouldBe true
+      Day2.isValidPart2Coord((3,0))   shouldBe false
+      Day2.isValidPart2Coord((4,0))   shouldBe false
+      Day2.isValidPart2Coord((2, -1)) shouldBe false
 
-      assert(!Day2.isValidPart2Coord((-1,1)))
-      assert(!Day2.isValidPart2Coord((0,1)))
-      assert(Day2.isValidPart2Coord((1,1)))
-      assert(Day2.isValidPart2Coord((2,1)))
-      assert(Day2.isValidPart2Coord((3,1)))
-      assert(!Day2.isValidPart2Coord(4, 1))
-      assert(!Day2.isValidPart2Coord(5, 1))
+      Day2.isValidPart2Coord((-1,1))  shouldBe false
+      Day2.isValidPart2Coord((0,1))   shouldBe false
+      Day2.isValidPart2Coord((1,1))   shouldBe true
+      Day2.isValidPart2Coord((2,1))   shouldBe true
+      Day2.isValidPart2Coord((3,1))   shouldBe true
+      Day2.isValidPart2Coord(4, 1)    shouldBe false
+      Day2.isValidPart2Coord(5, 1)    shouldBe false
 
-      assert(!Day2.isValidPart2Coord((2, -1)))
-      assert(Day2.isValidPart2Coord((2,0)))
-      assert(Day2.isValidPart2Coord((2,1)))
-      assert(Day2.isValidPart2Coord((2,2)))
-      assert(Day2.isValidPart2Coord((2,3)))
-      assert(Day2.isValidPart2Coord((2,4)))
-      assert(!Day2.isValidPart2Coord((2,5)))
+      Day2.isValidPart2Coord((2, -1)) shouldBe false
+      Day2.isValidPart2Coord((2,0))   shouldBe true
+      Day2.isValidPart2Coord((2,1))   shouldBe true
+      Day2.isValidPart2Coord((2,2))   shouldBe true
+      Day2.isValidPart2Coord((2,3))   shouldBe true
+      Day2.isValidPart2Coord((2,4))   shouldBe true
+      Day2.isValidPart2Coord((2,5))   shouldBe false
 
-      assert(!Day2.isValidPart2Coord((3, -1)))
-      assert(!Day2.isValidPart2Coord((3,0)))
-      assert(Day2.isValidPart2Coord((3,1)))
-      assert(Day2.isValidPart2Coord((3,2)))
-      assert(Day2.isValidPart2Coord((3,3)))
-      assert(!Day2.isValidPart2Coord((3,4)))
-      assert(!Day2.isValidPart2Coord((3,5)))
+      Day2.isValidPart2Coord((3, -1)) shouldBe false
+      Day2.isValidPart2Coord((3,0))   shouldBe false
+      Day2.isValidPart2Coord((3,1))   shouldBe true
+      Day2.isValidPart2Coord((3,2))   shouldBe true
+      Day2.isValidPart2Coord((3,3))   shouldBe true
+      Day2.isValidPart2Coord((3,4))   shouldBe false
+      Day2.isValidPart2Coord((3,5))   shouldBe false
 
-      assert(!Day2.isValidPart2Coord((4,0)))
-      assert(!Day2.isValidPart2Coord((4,1)))
-      assert(Day2.isValidPart2Coord((4,2)))
-      assert(!Day2.isValidPart2Coord((4,3)))
-      assert(!Day2.isValidPart2Coord((4,4)))
-      assert(!Day2.isValidPart2Coord((5,2)))
+      Day2.isValidPart2Coord((4,0))   shouldBe false
+      Day2.isValidPart2Coord((4,1))   shouldBe false
+      Day2.isValidPart2Coord((4,2))   shouldBe true
+      Day2.isValidPart2Coord((4,3))   shouldBe false
+      Day2.isValidPart2Coord((4,4))   shouldBe false
+      Day2.isValidPart2Coord((5,2))   shouldBe false
     }
 
     "from a series of moves" - {
       val seriesOfMoves = List("ULL", "RRDDD", "LURDL", "UUUUD")
       "gets multiple coordinates" in {
-        assert(Day2.getPart1Coordinates(seriesOfMoves) == List((0, 0),
-                                                               (2, 2),
-                                                               (1, 2),
-                                                               (1, 1)))
+        Day2.getPart1Coordinates(seriesOfMoves) shouldBe List((0, 0),
+                                                              (2, 2),
+                                                              (1, 2),
+                                                              (1, 1))
       }
       "gets multiple digits" in {
-        assert(Day2.getPart1Digits(seriesOfMoves) == "1985")
+        Day2.getPart1Digits(seriesOfMoves) shouldBe "1985"
       }
       "handles part 2 keyboard layout" in {
-        assert(Day2.getPart2Keys(seriesOfMoves) == "5DB3")
+        Day2.getPart2Keys(seriesOfMoves) shouldBe "5DB3"
       }
     }
 
     "solves" - {
-      val stream: InputStream = getClass.getResourceAsStream("input_day2")
+      val stream: InputStream = getClass.getResourceAsStream("input_day2.txt")
       val input = io.Source.fromInputStream(stream).mkString
       val inputList = input.lines.toList
       "part 1" in {
-        assert(Day2.getPart1Digits(inputList) == "65556")
+        Day2.getPart1Digits(inputList) shouldBe "65556"
       }
       "part 2" in {
-        assert(Day2.getPart2Keys(inputList) == "CB779")
+        Day2.getPart2Keys(inputList) shouldBe "CB779"
       }
     }
     "alternative solves" - {
-      val input = (getClass.getResourceAsStream("input_day2") |>
+      val input = (getClass.getResourceAsStream("input_day2.txt") |>
                     io.Source.fromInputStream).getLines.toList
       "part 1" in {
         val actual = input |> Day2Alternative.parseInstructions(Day2Alternative.Phone.Five)
-        assert(actual == List(Day2Alternative.Phone.Six,
-                              Day2Alternative.Phone.Five,
-                              Day2Alternative.Phone.Five,
-                              Day2Alternative.Phone.Five,
-                              Day2Alternative.Phone.Six))
+        actual shouldBe List(Day2Alternative.Phone.Six,
+                             Day2Alternative.Phone.Five,
+                             Day2Alternative.Phone.Five,
+                             Day2Alternative.Phone.Five,
+                             Day2Alternative.Phone.Six)
       }
       "part 2" in {
         val actual = input |> Day2Alternative.parseInstructions(Day2Alternative.Potty.Five)
-        assert(actual == List(Day2Alternative.Potty.C,
-                              Day2Alternative.Potty.B,
-                              Day2Alternative.Potty.Seven,
-                              Day2Alternative.Potty.Seven,
-                              Day2Alternative.Potty.Nine))
+        actual shouldBe List(Day2Alternative.Potty.C,
+                             Day2Alternative.Potty.B,
+                             Day2Alternative.Potty.Seven,
+                             Day2Alternative.Potty.Seven,
+                             Day2Alternative.Potty.Nine)
       }
     }
   }
