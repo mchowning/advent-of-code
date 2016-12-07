85c610656df4cabd155fa7d1c2e2530a506b1292
Matt Chowning <mchowning@gmail.com>
2016-12-06 21:37:19 -0500

    switch coordinates to be consistent with keypad numbering direction

--- 2016-scala/src/main/scala/Day2.scala
+++ 2016-scala/src/main/scala/Day2.scala
@@ -3,8 +3,14 @@ object Day2 {
 
   val keypadSize = 3
 
+  // keypad is laid out:
+  // 1 2 3
+  // 4 5 6
+  // 7 8 9
+
   // Coordinates represent the x,y coordinates on a keypad
-  // with 0,0 as the bottom left key
+  // with 0,0 as the top left key
+
   type Coordinate = (Int, Int)
 
   def move(coordinate: Coordinate, direction: Char): Coordinate = {
@@ -14,9 +20,9 @@ object Day2 {
 
     val (x,y) = coordinate
     direction match {
-      case 'U' => (x              , maxLimit(y + 1))
+      case 'U' => (x              , minLimit(y - 1))
       case 'R' => (maxLimit(x + 1), y              )
-      case 'D' => (x              , minLimit(y - 1))
+      case 'D' => (x              , maxLimit(y + 1))
       case 'L' => (minLimit(x - 1), y              )
     }
   }
@@ -27,6 +33,6 @@ object Day2 {
 
   def getDigit(coordinate: Coordinate): Int = {
     val (x,y) = coordinate
-    ((2-y) * 3) + x + 1
+    (y * 3) + x + 1
   }
 }
--- 2016-scala/src/test/scala/Day2Spec.scala
+++ 2016-scala/src/test/scala/Day2Spec.scala
@@ -4,43 +4,43 @@ class Day2Spec extends FreeSpec {
 
   "Day2" - {
     "handles non edge moves" in {
-      assert(Day2.move((1,1), 'U') == (1,2))
+      assert(Day2.move((1,1), 'U') == (1,0))
       assert(Day2.move((1,1), 'R') == (2,1))
-      assert(Day2.move((1,1), 'D') == (1,0))
+      assert(Day2.move((1,1), 'D') == (1,2))
       assert(Day2.move((1,1), 'L') == (0,1))
     }
     "handles edge moves" in {
       assert(Day2.move((0,0), 'L') == (0,0))
-      assert(Day2.move((0,0), 'D') == (0,0))
-      assert(Day2.move((1,0), 'D') == (1,0))
-      assert(Day2.move((2,0), 'D') == (2,0))
+      assert(Day2.move((0,0), 'U') == (0,0))
+      assert(Day2.move((1,0), 'U') == (1,0))
+      assert(Day2.move((2,0), 'U') == (2,0))
       assert(Day2.move((2,0), 'R') == (2,0))
 
       assert(Day2.move((0,1), 'L') == (0,1))
       assert(Day2.move((2,1), 'R') == (2,1))
 
       assert(Day2.move((0,2), 'L') == (0,2))
-      assert(Day2.move((0,2), 'U') == (0,2))
-      assert(Day2.move((1,2), 'U') == (1,2))
-      assert(Day2.move((2,2), 'U') == (2,2))
+      assert(Day2.move((0,2), 'D') == (0,2))
+      assert(Day2.move((1,2), 'D') == (1,2))
+      assert(Day2.move((2,2), 'D') == (2,2))
       assert(Day2.move((2,2), 'R') == (2,2))
     }
     "handles multiple moves" in {
-      assert(Day2.move((1,1), "ULL") == (0,2))
-      assert(Day2.move((0,2), "RRDDD") == (2,0))
-      assert(Day2.move((2,0), "LURDL") == (1,0))
-      assert(Day2.move((1,0), "UUUUD") == (1,1))
+      assert(Day2.move((1,1), "ULL") == (0,0))
+      assert(Day2.move((0,0), "RRDDD") == (2,2))
+      assert(Day2.move((2,2), "LURDL") == (1,2))
+      assert(Day2.move((1,2), "UUUUD") == (1,1))
     }
     "converts position to digit" in {
-      assert(Day2.getDigit((0,0)) == 7)
-      assert(Day2.getDigit((1,0)) == 8)
-      assert(Day2.getDigit((2,0)) == 9)
+      assert(Day2.getDigit((0,0)) == 1)
+      assert(Day2.getDigit((1,0)) == 2)
+      assert(Day2.getDigit((2,0)) == 3)
       assert(Day2.getDigit((0,1)) == 4)
       assert(Day2.getDigit((1,1)) == 5)
       assert(Day2.getDigit((2,1)) == 6)
-      assert(Day2.getDigit((0,2)) == 1)
-      assert(Day2.getDigit((1,2)) == 2)
-      assert(Day2.getDigit((2,2)) == 3)
+      assert(Day2.getDigit((0,2)) == 7)
+      assert(Day2.getDigit((1,2)) == 8)
+      assert(Day2.getDigit((2,2)) == 9)
     }
   }
 }
