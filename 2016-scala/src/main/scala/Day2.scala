65f7c2905c3bf1203e09591694ae3b6f324990dd
Matt Chowning <mchowning@gmail.com>
2016-12-07 06:54:43 -0500

    cleanup and organization

--- 2016-scala/src/main/scala/Day2.scala
+++ 2016-scala/src/main/scala/Day2.scala
@@ -17,38 +17,96 @@ object Day2 {
   //     D
 
   // Coordinates represent the x,y coordinates on a keypad
-  // with 0,0 as the top left key
+  // with 0,0 as the top left position
 
   type Coordinate = (Int, Int)
 
-  def moveToCoordinate(coordinate: Coordinate,
-                       direction: Char,
-                       isValidCoord: Coordinate => Boolean): Coordinate = {
-
-    val (x,y) = coordinate
-    val newCoordinate = direction match {
-      case 'U' => (x              , y - 1)
-      case 'R' => (x + 1, y              )
-      case 'D' => (x              , y + 1)
-      case 'L' => (x - 1, y              )
+  private def getCoordinateFromSingleMove(coordinate: Coordinate,
+                                          direction: Char,
+                                          isValidCoord: Coordinate => Boolean
+                                         ): Coordinate = {
+    val updatedCoordinate = {
+      val (x, y) = coordinate
+      direction match {
+        case 'U' => (x, y - 1)
+        case 'R' => (x + 1, y)
+        case 'D' => (x, y + 1)
+        case 'L' => (x - 1, y)
+      }
     }
-    if (isValidCoord(newCoordinate)) newCoordinate else coordinate
+
+    if (isValidCoord(updatedCoordinate)) updatedCoordinate else coordinate
   }
 
-  def moveToPart1Coordinate(coordinate: Coordinate, direction: Char): Coordinate =
-    moveToCoordinate(coordinate, direction, validCoordPart1)
+  private def getCoordinateFromMultipleMoves(coordinate: Coordinate,
+                                             directions: String,
+                                             singleMoveCoordinateGetter: (Coordinate, Char) => Coordinate
+                                            ): Coordinate = {
+    (coordinate /: directions) { (coord, d) => singleMoveCoordinateGetter(coord, d) }
+  }
 
-  def moveToPart2Coordinate(coordinate: Coordinate, direction: Char): Coordinate =
-    moveToCoordinate(coordinate, direction, validCoordPart2)
+  private def getCoordinates(moves: List[String],
+                             mover: (Coordinate, String) => Coordinate,
+                             startCoordinate: Coordinate): List[Coordinate] =
+    moves.scanLeft(startCoordinate)(mover)
+         .tail // drop start coordinate
 
-  private def validCoordPart1(coordinate: Coordinate): Boolean = coordinate match {
+  private def getKeys(moves: List[String],
+                      coordGetter: List[String] => List[Coordinate],
+                      keyNameGetter: Coordinate => String
+                     ): String =
+    coordGetter(moves)
+      .map(keyNameGetter)
+      .mkString
+
+  /*
+   * part 1
+   */
+
+  def getPart1Digits(moves: List[String]): String = {
+    val digitGetter = getPart1Digit _ andThen (_.toString)
+    getKeys(moves, getPart1Coordinates, digitGetter)
+  }
+
+  def getPart1Coordinates(moves: List[String]): List[Coordinate] =
+    getCoordinates(moves, getPart1CoordinateFromMultipleMoves, (1,1))
+
+  def getPart1CoordinateFromMultipleMoves(coordinate: Coordinate, directions: String): Coordinate = {
+    getCoordinateFromMultipleMoves(coordinate, directions, getPart1CoordinateFromSingleMove)
+  }
+
+  def getPart1CoordinateFromSingleMove(coordinate: Coordinate, direction: Char): Coordinate =
+    getCoordinateFromSingleMove(coordinate, direction, isValidPart1Coord)
+
+  def getPart1Digit(coordinate: Coordinate): Int = {
+    val (x,y) = coordinate
+    (y * 3) + x + 1
+  }
+
+  def isValidPart1Coord(coordinate: Coordinate): Boolean = coordinate match {
     case (x,y) => x >= 0 &&
                   y >= 0 &&
                   x < part1KeypadSize &&
                   y < part1KeypadSize
   }
 
-  def validCoordPart2(coordinate: Coordinate): Boolean = coordinate match {
+  /*
+   * part 2
+   */
+
+  def getPart2Keys(moves: List[String]): String =
+    getKeys(moves, getPart2Coordinates, getPart2Key)
+
+  def getPart2Coordinates(moves: List[String]): List[Coordinate] =
+    getCoordinates(moves, getPart2CoordinateFromMultipleMoves, (0,2))
+
+  def getPart2CoordinateFromMultipleMoves(coordinate: Coordinate, directions: String): Coordinate =
+    getCoordinateFromMultipleMoves(coordinate, directions, getPart2CoordinateFromSingleMove)
+
+  def getPart2CoordinateFromSingleMove(coordinate: Coordinate, direction: Char): Coordinate =
+    getCoordinateFromSingleMove(coordinate, direction, isValidPart2Coord)
+
+  def isValidPart2Coord(coordinate: Coordinate): Boolean = coordinate match {
     case (x,0) => x == 2
     case (x,1) => x > 0 && x < 4
     case (x,2) => x >= 0 && x <= 4
@@ -57,56 +115,19 @@ object Day2 {
     case _     => false
   }
 
-  def moveToPart1Coordinate(coordinate: Coordinate, directions: String): Coordinate = {
-    (coordinate /: directions) { (coord, d) => moveToPart1Coordinate(coord, d) }
-  }
-
-  def moveToPart2Coordinate(coordinate: Coordinate, directions: String): Coordinate = {
-    (coordinate /: directions) { (coord, d) => moveToPart2Coordinate(coord, d) } // TODO duplication
-  }
-
-  def getPart1Digit(coordinate: Coordinate): Int = {
-    val (x,y) = coordinate
-    (y * 3) + x + 1
-  }
-
-  private def getCoordinates(moves: List[String],
-                             mover: (Coordinate, String) => Coordinate,
-                             startCoordinate: Coordinate): List[Coordinate] =
-    moves.scanLeft(startCoordinate)(mover)
-         .tail
-
-  def getPart1Coordinates(moves: List[String]): List[Coordinate] =
-    getCoordinates(moves, moveToPart1Coordinate, (1,1))
-
-  def getPart2Coordinates(moves: List[String]): List[Coordinate] =
-    getCoordinates(moves, moveToPart2Coordinate, (0,2))
-
-  def getPart1Digits(moves: List[String]): String = {
-    getPart1Coordinates(moves)
-    .map(getPart1Digit)
-    .mkString
-  }
-
-  def getPart2Key(coordinate: Coordinate): Char = coordinate match {
-    case (2,0) => '1'
-    case (1,1) => '2'
-    case (2,1) => '3'
-    case (3,1) => '4'
-    case (0,2) => '5'
-    case (1,2) => '6'
-    case (2,2) => '7'
-    case (3,2) => '8'
-    case (4,2) => '9'
-    case (1,3) => 'A'
-    case (2,3) => 'B'
-    case (3,3) => 'C'
-    case (2,4) => 'D'
-  }
-
-  def getPart2Keys(moves: List[String]): String = {
-    getPart2Coordinates(moves)
-      .map(getPart2Key)
-      .mkString
+  def getPart2Key(coordinate: Coordinate): String = coordinate match {
+    case (2,0) => "1"
+    case (1,1) => "2"
+    case (2,1) => "3"
+    case (3,1) => "4"
+    case (0,2) => "5"
+    case (1,2) => "6"
+    case (2,2) => "7"
+    case (3,2) => "8"
+    case (4,2) => "9"
+    case (1,3) => "A"
+    case (2,3) => "B"
+    case (3,3) => "C"
+    case (2,4) => "D"
   }
 }
--- 2016-scala/src/test/scala/Day2Spec.scala
+++ 2016-scala/src/test/scala/Day2Spec.scala
@@ -6,32 +6,32 @@ class Day2Spec extends FreeSpec {
 
   "Day2" - {
     "handles non edge moves" in {
-      assert(Day2.moveToPart1Coordinate((1,1), 'U') ==(1,0))
-      assert(Day2.moveToPart1Coordinate((1,1), 'R') ==(2,1))
-      assert(Day2.moveToPart1Coordinate((1,1), 'D') ==(1,2))
-      assert(Day2.moveToPart1Coordinate((1,1), 'L') ==(0,1))
+      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'U') ==(1,0))
+      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'R') ==(2,1))
+      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'D') ==(1,2))
+      assert(Day2.getPart1CoordinateFromSingleMove((1,1), 'L') ==(0,1))
     }
     "handles edge moves" in {
-      assert(Day2.moveToPart1Coordinate((0,0), 'L') ==(0,0))
-      assert(Day2.moveToPart1Coordinate((0,0), 'U') ==(0,0))
-      assert(Day2.moveToPart1Coordinate((1,0), 'U') ==(1,0))
-      assert(Day2.moveToPart1Coordinate((2,0), 'U') ==(2,0))
-      assert(Day2.moveToPart1Coordinate((2,0), 'R') ==(2,0))
+      assert(Day2.getPart1CoordinateFromSingleMove((0,0), 'L') ==(0,0))
+      assert(Day2.getPart1CoordinateFromSingleMove((0,0), 'U') ==(0,0))
+      assert(Day2.getPart1CoordinateFromSingleMove((1,0), 'U') ==(1,0))
+      assert(Day2.getPart1CoordinateFromSingleMove((2,0), 'U') ==(2,0))
+      assert(Day2.getPart1CoordinateFromSingleMove((2,0), 'R') ==(2,0))
 
-      assert(Day2.moveToPart1Coordinate((0,1), 'L') ==(0,1))
-      assert(Day2.moveToPart1Coordinate((2,1), 'R') ==(2,1))
+      assert(Day2.getPart1CoordinateFromSingleMove((0,1), 'L') ==(0,1))
+      assert(Day2.getPart1CoordinateFromSingleMove((2,1), 'R') ==(2,1))
 
-      assert(Day2.moveToPart1Coordinate((0,2), 'L') ==(0,2))
-      assert(Day2.moveToPart1Coordinate((0,2), 'D') ==(0,2))
-      assert(Day2.moveToPart1Coordinate((1,2), 'D') ==(1,2))
-      assert(Day2.moveToPart1Coordinate((2,2), 'D') ==(2,2))
-      assert(Day2.moveToPart1Coordinate((2,2), 'R') ==(2,2))
+      assert(Day2.getPart1CoordinateFromSingleMove((0,2), 'L') ==(0,2))
+      assert(Day2.getPart1CoordinateFromSingleMove((0,2), 'D') ==(0,2))
+      assert(Day2.getPart1CoordinateFromSingleMove((1,2), 'D') ==(1,2))
+      assert(Day2.getPart1CoordinateFromSingleMove((2,2), 'D') ==(2,2))
+      assert(Day2.getPart1CoordinateFromSingleMove((2,2), 'R') ==(2,2))
     }
     "handles multiple moves" in {
-      assert(Day2.moveToPart1Coordinate((1,1), "ULL") ==(0,0))
-      assert(Day2.moveToPart1Coordinate((0,0), "RRDDD") ==(2,2))
-      assert(Day2.moveToPart1Coordinate((2,2), "LURDL") ==(1,2))
-      assert(Day2.moveToPart1Coordinate((1,2), "UUUUD") ==(1,1))
+      assert(Day2.getPart1CoordinateFromMultipleMoves((1,1), "ULL") ==(0,0))
+      assert(Day2.getPart1CoordinateFromMultipleMoves((0,0), "RRDDD") ==(2,2))
+      assert(Day2.getPart1CoordinateFromMultipleMoves((2,2), "LURDL") ==(1,2))
+      assert(Day2.getPart1CoordinateFromMultipleMoves((1,2), "UUUUD") ==(1,1))
     }
     "converts position to digit" in {
       assert(Day2.getPart1Digit((0,0)) == 1)
@@ -44,46 +44,71 @@ class Day2Spec extends FreeSpec {
       assert(Day2.getPart1Digit((1,2)) == 8)
       assert(Day2.getPart1Digit((2,2)) == 9)
     }
+    "determines valid coordinates for part 1" in {
+      assert(!Day2.isValidPart1Coord((0, -1)))
+      assert(!Day2.isValidPart1Coord((1, -1)))
+      assert(!Day2.isValidPart1Coord((2, -1)))
+      assert(!Day2.isValidPart1Coord((-1,0)))
+      assert(!Day2.isValidPart1Coord((3,0)))
+      assert(!Day2.isValidPart1Coord((-1,1)))
+      assert(!Day2.isValidPart1Coord((3,1)))
+      assert(!Day2.isValidPart1Coord((-1,2)))
+      assert(!Day2.isValidPart1Coord((3,2)))
+      assert(!Day2.isValidPart1Coord((0,3)))
+      assert(!Day2.isValidPart1Coord((1,3)))
+      assert(!Day2.isValidPart1Coord((2,3)))
+
+      assert(Day2.isValidPart1Coord((0,0)))
+      assert(Day2.isValidPart1Coord((1,0)))
+      assert(Day2.isValidPart1Coord((2,0)))
+      assert(Day2.isValidPart1Coord((0,1)))
+      assert(Day2.isValidPart1Coord((1,1)))
+      assert(Day2.isValidPart1Coord((2,1)))
+      assert(Day2.isValidPart1Coord((0,1)))
+      assert(Day2.isValidPart1Coord((1,1)))
+      assert(Day2.isValidPart1Coord((2,1)))
+    }
     "determines valid coordinates for part 2" in {
 
-      assert(!Day2.validCoordPart2((0,0)))
-      assert(!Day2.validCoordPart2((1,0)))
-      assert(Day2.validCoordPart2((2,0)))
-      assert(!Day2.validCoordPart2((3,0)))
-      assert(!Day2.validCoordPart2((4,0)))
-      assert(!Day2.validCoordPart2((2,-1)))
+      assert(!Day2.isValidPart2Coord((0,0)))
+      assert(!Day2.isValidPart2Coord((1,0)))
+      assert(Day2.isValidPart2Coord((2,0)))
+      assert(!Day2.isValidPart2Coord((3,0)))
+      assert(!Day2.isValidPart2Coord((4,0)))
+      assert(!Day2.isValidPart2Coord((2, -1)))
 
-      assert(!Day2.validCoordPart2((-1,1)))
-      assert(!Day2.validCoordPart2((0,1)))
-      assert(Day2.validCoordPart2((1,1)))
-      assert(Day2.validCoordPart2((2,1)))
-      assert(Day2.validCoordPart2((3,1)))
-      assert(!Day2.validCoordPart2(4,1))
-      assert(!Day2.validCoordPart2(5,1))
+      assert(!Day2.isValidPart2Coord((-1,1)))
+      assert(!Day2.isValidPart2Coord((0,1)))
+      assert(Day2.isValidPart2Coord((1,1)))
+      assert(Day2.isValidPart2Coord((2,1)))
+      assert(Day2.isValidPart2Coord((3,1)))
+      assert(!Day2.isValidPart2Coord(4, 1))
+      assert(!Day2.isValidPart2Coord(5, 1))
 
-      assert(!Day2.validCoordPart2((2,-1)))
-      assert(Day2.validCoordPart2((2,0)))
-      assert(Day2.validCoordPart2((2,1)))
-      assert(Day2.validCoordPart2((2,2)))
-      assert(Day2.validCoordPart2((2,3)))
-      assert(Day2.validCoordPart2((2,4)))
-      assert(!Day2.validCoordPart2((2,5)))
+      assert(!Day2.isValidPart2Coord((2, -1)))
+      assert(Day2.isValidPart2Coord((2,0)))
+      assert(Day2.isValidPart2Coord((2,1)))
+      assert(Day2.isValidPart2Coord((2,2)))
+      assert(Day2.isValidPart2Coord((2,3)))
+      assert(Day2.isValidPart2Coord((2,4)))
+      assert(!Day2.isValidPart2Coord((2,5)))
 
-      assert(!Day2.validCoordPart2((3,-1)))
-      assert(!Day2.validCoordPart2((3,0)))
-      assert(Day2.validCoordPart2((3,1)))
-      assert(Day2.validCoordPart2((3,2)))
-      assert(Day2.validCoordPart2((3,3)))
-      assert(!Day2.validCoordPart2((3,4)))
-      assert(!Day2.validCoordPart2((3,5)))
+      assert(!Day2.isValidPart2Coord((3, -1)))
+      assert(!Day2.isValidPart2Coord((3,0)))
+      assert(Day2.isValidPart2Coord((3,1)))
+      assert(Day2.isValidPart2Coord((3,2)))
+      assert(Day2.isValidPart2Coord((3,3)))
+      assert(!Day2.isValidPart2Coord((3,4)))
+      assert(!Day2.isValidPart2Coord((3,5)))
 
-      assert(!Day2.validCoordPart2((4,0)))
-      assert(!Day2.validCoordPart2((4,1)))
-      assert(Day2.validCoordPart2((4,2)))
-      assert(!Day2.validCoordPart2((4,3)))
-      assert(!Day2.validCoordPart2((4,4)))
-      assert(!Day2.validCoordPart2((5,2)))
+      assert(!Day2.isValidPart2Coord((4,0)))
+      assert(!Day2.isValidPart2Coord((4,1)))
+      assert(Day2.isValidPart2Coord((4,2)))
+      assert(!Day2.isValidPart2Coord((4,3)))
+      assert(!Day2.isValidPart2Coord((4,4)))
+      assert(!Day2.isValidPart2Coord((5,2)))
     }
+
     "from a series of moves" - {
       val seriesOfMoves = List("ULL", "RRDDD", "LURDL", "UUUUD")
       "gets multiple coordinates" in {
@@ -99,6 +124,7 @@ class Day2Spec extends FreeSpec {
         assert(Day2.getPart2Keys(seriesOfMoves) == "5DB3")
       }
     }
+
     "solves" - {
       val stream: InputStream = getClass.getResourceAsStream("input_day2")
       val input = io.Source.fromInputStream(stream).mkString
