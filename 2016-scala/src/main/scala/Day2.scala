e28486310a3019591f50ff05e2555600ce6fa26b
Matt Chowning <mchowning@gmail.com>
2016-12-06 22:01:29 -0500

    prepare for part 2

--- 2016-scala/src/main/scala/Day2.scala
+++ 2016-scala/src/main/scala/Day2.scala
@@ -1,50 +1,62 @@
 
+
 object Day2 {
 
-  val keypadSize = 3
+  val part1KeypadSize = 3
 
-  // keypad is laid out:
+  // part 1 keypad is laid out:
   // 1 2 3
   // 4 5 6
   // 7 8 9
+
+  // part 2 keypad is laid out:
+  //     1
+  //   2 3 4
+  // 5 6 7 8 9
+  //   A B C
+  //     D
 
   // Coordinates represent the x,y coordinates on a keypad
   // with 0,0 as the top left key
 
   type Coordinate = (Int, Int)
 
-  def moveToCoordinate(coordinate: Coordinate, direction: Char): Coordinate = {
+  def moveToPart1Coordinate(coordinate: Coordinate, direction: Char): Coordinate = {
 
-    def maxLimit(i: Int): Int = Math.min(i, keypadSize - 1)
-    def minLimit(i: Int): Int = Math.max(i, 0)
+    def validCoord(coordinate: Coordinate): Boolean = coordinate match {
+      case (x,y) => x >= 0 &&
+                      y >= 0 &&
+                      x < part1KeypadSize &&
+                      y < part1KeypadSize
+    }
 
     val (x,y) = coordinate
-    direction match {
-      case 'U' => (x              , minLimit(y - 1))
-      case 'R' => (maxLimit(x + 1), y              )
-      case 'D' => (x              , maxLimit(y + 1))
-      case 'L' => (minLimit(x - 1), y              )
+    val newCoordinate = direction match {
+      case 'U' => (x              , y - 1)
+      case 'R' => (x + 1, y              )
+      case 'D' => (x              , y + 1)
+      case 'L' => (x - 1, y              )
     }
+    if (validCoord(newCoordinate)) newCoordinate else coordinate
   }
 
-  def moveToCoordinate(coordinate: Coordinate, directions: String): Coordinate = {
-    (coordinate /: directions) { (coord, d) => moveToCoordinate(coord, d) }
+  def moveToPart1Coordinate(coordinate: Coordinate, directions: String): Coordinate = {
+    (coordinate /: directions) { (coord, d) => moveToPart1Coordinate(coord, d) }
   }
 
-  def getDigit(coordinate: Coordinate): Int = {
+  def getPart1Digit(coordinate: Coordinate): Int = {
     val (x,y) = coordinate
     (y * 3) + x + 1
   }
 
-  def getCoordinates(moves: List[String]): List[Coordinate] = {
-    moves.scanLeft((1,1))(moveToCoordinate)
+  def getPart1Coordinates(moves: List[String]): List[Coordinate] = {
+    moves.scanLeft((1,1))(moveToPart1Coordinate)
          .tail
   }
 
-  def getDigits(moves: List[String]): Int = {
-    getCoordinates(moves)
-      .map(getDigit)
-      .mkString
-      .toInt
+  def getDigits(moves: List[String]): String = {
+    getPart1Coordinates(moves)
+    .map(getPart1Digit)
+    .mkString
   }
 }
--- 2016-scala/src/test/scala/Day2Spec.scala
+++ 2016-scala/src/test/scala/Day2Spec.scala
@@ -6,61 +6,61 @@ class Day2Spec extends FreeSpec {
 
   "Day2" - {
     "handles non edge moves" in {
-      assert(Day2.moveToCoordinate((1,1), 'U') ==(1,0))
-      assert(Day2.moveToCoordinate((1,1), 'R') ==(2,1))
-      assert(Day2.moveToCoordinate((1,1), 'D') ==(1,2))
-      assert(Day2.moveToCoordinate((1,1), 'L') ==(0,1))
+      assert(Day2.moveToPart1Coordinate((1,1), 'U') ==(1,0))
+      assert(Day2.moveToPart1Coordinate((1,1), 'R') ==(2,1))
+      assert(Day2.moveToPart1Coordinate((1,1), 'D') ==(1,2))
+      assert(Day2.moveToPart1Coordinate((1,1), 'L') ==(0,1))
     }
     "handles edge moves" in {
-      assert(Day2.moveToCoordinate((0,0), 'L') ==(0,0))
-      assert(Day2.moveToCoordinate((0,0), 'U') ==(0,0))
-      assert(Day2.moveToCoordinate((1,0), 'U') ==(1,0))
-      assert(Day2.moveToCoordinate((2,0), 'U') ==(2,0))
-      assert(Day2.moveToCoordinate((2,0), 'R') ==(2,0))
+      assert(Day2.moveToPart1Coordinate((0,0), 'L') ==(0,0))
+      assert(Day2.moveToPart1Coordinate((0,0), 'U') ==(0,0))
+      assert(Day2.moveToPart1Coordinate((1,0), 'U') ==(1,0))
+      assert(Day2.moveToPart1Coordinate((2,0), 'U') ==(2,0))
+      assert(Day2.moveToPart1Coordinate((2,0), 'R') ==(2,0))
 
-      assert(Day2.moveToCoordinate((0,1), 'L') ==(0,1))
-      assert(Day2.moveToCoordinate((2,1), 'R') ==(2,1))
+      assert(Day2.moveToPart1Coordinate((0,1), 'L') ==(0,1))
+      assert(Day2.moveToPart1Coordinate((2,1), 'R') ==(2,1))
 
-      assert(Day2.moveToCoordinate((0,2), 'L') ==(0,2))
-      assert(Day2.moveToCoordinate((0,2), 'D') ==(0,2))
-      assert(Day2.moveToCoordinate((1,2), 'D') ==(1,2))
-      assert(Day2.moveToCoordinate((2,2), 'D') ==(2,2))
-      assert(Day2.moveToCoordinate((2,2), 'R') ==(2,2))
+      assert(Day2.moveToPart1Coordinate((0,2), 'L') ==(0,2))
+      assert(Day2.moveToPart1Coordinate((0,2), 'D') ==(0,2))
+      assert(Day2.moveToPart1Coordinate((1,2), 'D') ==(1,2))
+      assert(Day2.moveToPart1Coordinate((2,2), 'D') ==(2,2))
+      assert(Day2.moveToPart1Coordinate((2,2), 'R') ==(2,2))
     }
     "handles multiple moves" in {
-      assert(Day2.moveToCoordinate((1,1), "ULL") ==(0,0))
-      assert(Day2.moveToCoordinate((0,0), "RRDDD") ==(2,2))
-      assert(Day2.moveToCoordinate((2,2), "LURDL") ==(1,2))
-      assert(Day2.moveToCoordinate((1,2), "UUUUD") ==(1,1))
+      assert(Day2.moveToPart1Coordinate((1,1), "ULL") ==(0,0))
+      assert(Day2.moveToPart1Coordinate((0,0), "RRDDD") ==(2,2))
+      assert(Day2.moveToPart1Coordinate((2,2), "LURDL") ==(1,2))
+      assert(Day2.moveToPart1Coordinate((1,2), "UUUUD") ==(1,1))
     }
     "converts position to digit" in {
-      assert(Day2.getDigit((0,0)) == 1)
-      assert(Day2.getDigit((1,0)) == 2)
-      assert(Day2.getDigit((2,0)) == 3)
-      assert(Day2.getDigit((0,1)) == 4)
-      assert(Day2.getDigit((1,1)) == 5)
-      assert(Day2.getDigit((2,1)) == 6)
-      assert(Day2.getDigit((0,2)) == 7)
-      assert(Day2.getDigit((1,2)) == 8)
-      assert(Day2.getDigit((2,2)) == 9)
+      assert(Day2.getPart1Digit((0,0)) == 1)
+      assert(Day2.getPart1Digit((1,0)) == 2)
+      assert(Day2.getPart1Digit((2,0)) == 3)
+      assert(Day2.getPart1Digit((0,1)) == 4)
+      assert(Day2.getPart1Digit((1,1)) == 5)
+      assert(Day2.getPart1Digit((2,1)) == 6)
+      assert(Day2.getPart1Digit((0,2)) == 7)
+      assert(Day2.getPart1Digit((1,2)) == 8)
+      assert(Day2.getPart1Digit((2,2)) == 9)
     }
     "from a series of moves" - {
       val seriesOfMoves = List("ULL", "RRDDD", "LURDL", "UUUUD")
       "gets multiple coordinates" in {
-        assert(Day2.getCoordinates(seriesOfMoves) == List((0, 0),
-                                                          (2, 2),
-                                                          (1, 2),
-                                                          (1, 1)))
+        assert(Day2.getPart1Coordinates(seriesOfMoves) == List((0, 0),
+                                                               (2, 2),
+                                                               (1, 2),
+                                                               (1, 1)))
       }
       "gets multiple digits" in {
-        assert(Day2.getDigits(seriesOfMoves) == 1985)
+        assert(Day2.getDigits(seriesOfMoves) == "1985")
       }
     }
     "solves part 1" in {
       val stream: InputStream = getClass.getResourceAsStream("input_day2")
       val input = io.Source.fromInputStream(stream).mkString
       val inputList = input.lines.toList
-      assert(Day2.getDigits(inputList) == 65556)
+      assert(Day2.getDigits(inputList) == "65556")
     }
   }
 }
