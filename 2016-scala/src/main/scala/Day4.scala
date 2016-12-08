40b2a62c63d9465a86e71233a5f50ca0fa1259b2
Matt Chowning <mchowning@gmail.com>
2016-12-07 23:13:14 -0500

    parse sector id (number)

--- 2016-scala/src/main/scala/Day4.scala
+++ 2016-scala/src/main/scala/Day4.scala
@@ -5,9 +5,10 @@ case class Room(encryptedName: String, sectorId: Int, checksum: String)
 object RoomParser extends RegexParsers {
 
   def name: Parser[String] = """[a-z\-]+""".r ^^ { _.toString.init }
+  def number: Parser[Int] = """[0-9]*""".r ^^ {_.toInt }
 
-  def full: Parser[Room] = name ^^ {
-    case name => new Room(name, 0, "")
+  def full: Parser[Room] = name ~ number ^^ {
+    case name ~ number => new Room(name, number, "")
   }
 
 }
--- 2016-scala/src/test/scala/Day4Spec.scala
+++ 2016-scala/src/test/scala/Day4Spec.scala
@@ -2,9 +2,11 @@ import org.scalatest.{Matchers, FreeSpec}
 
 class Day4Spec extends FreeSpec with Matchers {
 
-  "Day4" - {
-    "can parse name" in {
-      Day4.parseRoom("aaaaa-bbb-z-y-x-123[abxyz]").encryptedName shouldBe "aaaaa-bbb-z-y-x"
+  "Day4 can" - {
+    "parse Room" in {
+      val parsedRoom = Day4.parseRoom("aaaaa-bbb-z-y-x-123[abxyz]")
+      parsedRoom.encryptedName shouldBe "aaaaa-bbb-z-y-x"
+      parsedRoom.sectorId shouldBe 123
     }
   }
 
