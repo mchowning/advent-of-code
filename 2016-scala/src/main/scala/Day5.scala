c0563189e3c4423e00b125ad6c1998abfd9b3ddf
Matt Chowning <mchowning@gmail.com>
2016-12-08 23:07:03 -0500

    create md5 hash iterator

--- 2016-scala/src/main/scala/Day5.scala
+++ 2016-scala/src/main/scala/Day5.scala
@@ -1,11 +1,16 @@
 import java.security.MessageDigest
+import util.PipeOps._
 
 object Day5 {
 
-  def md5(s: String) = {
+  def md5(s: String): String = {
     MessageDigest.getInstance("MD5")
       .digest(s.getBytes)
       .map("%02X".format(_))
       .mkString
   }
+
+  def hashesFromPrefixedInput(inputPrefix: String): Iterator[String] =
+    Iterator.from(0)
+      .map(inputPrefix + _ |> md5)
 }
--- 2016-scala/src/test/scala/Day5Spec.scala
+++ 2016-scala/src/test/scala/Day5Spec.scala
@@ -5,10 +5,15 @@ class Day5Spec extends FreeSpec with Matchers {
   val inputPart1 = "abbhdwsy"
 
   "Day5" - {
-    "can get md5 hash" in {
+    "gets md5 hash" in {
       Day5.md5("abc3231929") shouldBe "00000155F8105DFF7F56EE10FA9B9ABD"
       Day5.md5("abc5017308") shouldBe "000008F82C5B3924A1ECBEBF60344E00"
       Day5.md5("abc5278568") shouldBe "00000F9A2C309875E05C5A5D09F1B8C4"
+    }
+    "generates md5 hashes for successive inputs with base abc" in {
+      Day5.hashesFromPrefixedInput("abc").take(3).toList shouldBe List("577571BE4DE9DCCE85A041BA0410F29F",
+                                                                       "23734CD52AD4A4FB877D8A1E26E5DF5F",
+                                                                       "63872B5565B2179BD72EA9C339192543")
     }
   }
 }
