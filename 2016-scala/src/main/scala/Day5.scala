3827deb3bca2be24157fff543c3be07dadb54140
Matt Chowning <mchowning@gmail.com>
2016-12-08 23:17:26 -0500

    Add method for getting first n hashes with prefix

--- 2016-scala/src/main/scala/Day5.scala
+++ 2016-scala/src/main/scala/Day5.scala
@@ -3,14 +3,24 @@ import util.PipeOps._
 
 object Day5 {
 
-  def md5(s: String): String = {
+  val inputPrefix = "abbhdwsy"
+
+  type Md5Hash = String
+
+  def md5(s: String): Md5Hash = {
     MessageDigest.getInstance("MD5")
       .digest(s.getBytes)
       .map("%02X".format(_))
       .mkString
   }
 
-  def hashesFromPrefixedInput(inputPrefix: String): Iterator[String] =
+  def hashesFromPrefixedInput: Iterator[Md5Hash] =
     Iterator.from(0)
       .map(inputPrefix + _ |> md5)
+
+  def getFirstNHashesWithPrefix(hashPrefix: String, numToTake: Int): List[Md5Hash] =
+    hashesFromPrefixedInput
+      .filter(_.startsWith(hashPrefix))
+      .take(numToTake)
+      .toList
 }
--- 2016-scala/src/test/scala/Day5Spec.scala
+++ 2016-scala/src/test/scala/Day5Spec.scala
@@ -2,8 +2,6 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class Day5Spec extends FreeSpec with Matchers {
 
-  val inputPart1 = "abbhdwsy"
-
   "Day5" - {
     "gets md5 hash" in {
       Day5.md5("abc3231929") shouldBe "00000155F8105DFF7F56EE10FA9B9ABD"
@@ -11,9 +9,14 @@ class Day5Spec extends FreeSpec with Matchers {
       Day5.md5("abc5278568") shouldBe "00000F9A2C309875E05C5A5D09F1B8C4"
     }
     "generates md5 hashes for successive inputs with base abc" in {
-      Day5.hashesFromPrefixedInput("abc").take(3).toList shouldBe List("577571BE4DE9DCCE85A041BA0410F29F",
-                                                                       "23734CD52AD4A4FB877D8A1E26E5DF5F",
-                                                                       "63872B5565B2179BD72EA9C339192543")
+      Day5.hashesFromPrefixedInput.take(3).toList shouldBe List("7E51386949E56DDAB4F31C503DE50F83",
+                                                                "917B4F767F6713624AE0E4B4A4CD3CC9",
+                                                                "1E2EC6125CC3E05CFD556134AE10E8AC")
+    }
+    "gets first 3 hashes with prefix 00" in {
+      Day5.getFirstNHashesWithPrefix("00", 3) shouldBe List("00B98B3BA4DF35F1E8DEEA2E508638D9",
+                                                            "004728CB6167C20592C4F9B681630C8C",
+                                                            "0029840BEE223B0229715192ED9DDEF5")
     }
   }
 }
