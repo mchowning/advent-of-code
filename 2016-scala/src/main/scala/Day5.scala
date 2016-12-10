216072f4877ce721ddcfd7a1ab9ad5574bf1c929
Matt Chowning <mchowning@gmail.com>
2016-12-10 14:57:44 -0500

    finished day 5

--- 2016-scala/src/main/scala/Day5.scala
+++ 2016-scala/src/main/scala/Day5.scala
@@ -1,47 +1,57 @@
 import java.security.MessageDigest
-import util.PipeOps._
 
 class Day5(inputPrefix: String) {
 
   type Md5Hash = String
 
-  def md5(s: String): Md5Hash = {
-    MessageDigest.getInstance("MD5")
-      .digest(s.getBytes)
-      .map("%02X".format(_))
-      .mkString
+  def hashesStartingWithFiveZerosFromPrefixedInput: Iterator[Md5Hash] = {
+
+    // each hex digit takes 4 bits, so 5 take 20 bits, or 2.5 bytes
+    // binary of 11110000 (0xF0), will bitwise & to 0 with anything that has the first 4 bits set to 0
+    def first5HexAre0(arrayOfBytes: Array[Byte]): Boolean =
+      arrayOfBytes(0) == 0 && arrayOfBytes(1) == 0 && (arrayOfBytes(2) & 0xF0) == 0
+
+    Iterator.from(0)
+      .map(i => MessageDigest.getInstance("MD5")
+                             .digest((inputPrefix + i).getBytes))
+      .filter(first5HexAre0)
+      .map(_.map("%02X".format(_))
+            .mkString)
   }
 
-  def hashesFromPrefixedInput: Iterator[Md5Hash] =
-    Iterator.from(0)
-      .map(inputPrefix + _ |> md5)
-
-  def getFirstNHashesWithPrefix(hashPrefix: String, numToTake: Int): List[Md5Hash] =
-    hashesFromPrefixedInput
-      .filter(_.startsWith(hashPrefix))
-      .take(numToTake)
-      .toList
+  private def getPart1Char(md5Hash: Md5Hash): Char = md5Hash.charAt(5)
 
   def getPart1Password: String = {
-    getFirstNHashesWithPrefix("00000", 8)
-      .map(_.charAt(5))
+    hashesStartingWithFiveZerosFromPrefixedInput
+      .take(8)
+      .map(getPart1Char)
       .mkString
   }
 
-  def getPart2Password(hashes: List[Md5Hash]): String = {
-    def processHash(hash: Md5Hash): (Int, Char) = {
-      val index = hash.charAt(5).toInt
-      val char = hash.charAt(6)
-      (index, char)
+  private def getPart2Index(md5Hash: Md5Hash): Int = md5Hash.charAt(5).asDigit
+  private def getPart2Char(md5Hash: Md5Hash): Char = md5Hash.charAt(6)
+
+  def getPart2Password: String = {
+    val emtpyArray = Array.fill[Option[Char]](8)(None)
+    hashesStartingWithFiveZerosFromPrefixedInput.scanLeft(emtpyArray) { (array, m) =>
+      val index = getPart2Index(m)
+      val hashMatchesEmptyPosition = index < array.length && array(index).isEmpty
+      if (hashMatchesEmptyPosition) array(index) = Some(getPart2Char(m))
+      array
     }
-
-    hashes.map(processHash)
-      .groupBy(_._1)
-      .values
-      .map(_.head)
-      .toSeq
-      .sortBy(_._1)
-      .map(_._2)
-      .mkString
+    .filter(_.forall(_.isDefined))
+    .next
+    .map(_.get)
+    .mkString
   }
+
+  //   inefficient, but seems easier to follow
+  //  def getPart2Password: String = {
+  //    (0 until 8).map { index =>
+  //      hashesFromPrefixedInput
+  //        .filter(getPart2Index(_) == index)
+  //        .next()
+  //    }.map(getPart2Char)
+  //      .mkString
+  //  }
 }
--- 2016-scala/src/test/scala/Day5Spec.scala
+++ 2016-scala/src/test/scala/Day5Spec.scala
@@ -5,52 +5,11 @@ class Day5Spec extends FreeSpec with Matchers {
   val testInputPrefix = "abbhdwsy"
 
   "Day5" - {
-    "gets md5 hash" in {
-      val subject = new Day5("")
-      subject.md5("abc3231929") shouldBe "00000155F8105DFF7F56EE10FA9B9ABD"
-      subject.md5("abc5017308") shouldBe "000008F82C5B3924A1ECBEBF60344E00"
-      subject.md5("abc5278568") shouldBe "00000F9A2C309875E05C5A5D09F1B8C4"
-    }
-    "generates md5 hashes for successive inputs with base abc" in {
-      new Day5(testInputPrefix)
-        .hashesFromPrefixedInput
-        .take(3)
-        .toList shouldBe List("7E51386949E56DDAB4F31C503DE50F83",
-                              "917B4F767F6713624AE0E4B4A4CD3CC9",
-                              "1E2EC6125CC3E05CFD556134AE10E8AC")
-    }
-    "gets first 3 hashes with prefix 00" in {
-      new Day5(testInputPrefix)
-        .getFirstNHashesWithPrefix("00", 3) shouldBe List("00B98B3BA4DF35F1E8DEEA2E508638D9",
-                                                          "004728CB6167C20592C4F9B681630C8C",
-                                                          "0029840BEE223B0229715192ED9DDEF5")
-    }
-    "find subsequent hashes with prefix" - {
-      val subject = new Day5("abc")
-      "easy example" in {
-        subject.getFirstNHashesWithPrefix("00", 1).head shouldBe "0034E0923CC38887A57BD7B1D4F953DF"
-      }
-      "tough examples" ignore {
-        subject.getFirstNHashesWithPrefix("00000", 2) shouldBe List("00000155F8105DFF7F56EE10FA9B9ABD",
-                                                                    "000008F82C5B3924A1ECBEBF60344E00")
-      }
-    }
-    "solve part 1" ignore {
+    "solves part 1" in {
       new Day5(testInputPrefix).getPart1Password shouldBe "801B56A7"
     }
-    "extracts part 2 password from list of hashes" - {
-      val subject = new Day5("")
-      "with a straightforward case" in {
-        subject.getPart2Password(List("000003A",
-                                      "000001R",
-                                      "0000025aaa")) shouldBe "R5A"
-      }
-      "by taking first result for a position" in {
-        subject.getPart2Password(List("000003A",
-                                      "000001R",
-                                      "0000025",
-                                      "000001T")) shouldBe "R5A"
-      }
+    "solves part 2" in {
+      new Day5(testInputPrefix).getPart2Password shouldBe "424A0197"
     }
   }
 }
