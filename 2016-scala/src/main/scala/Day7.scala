a8524239bb7521b7315ce371e8c2258361ae659a
Matt Chowning <mchowning@gmail.com>
2016-12-10 22:35:23 -0500

    check longer sequences

--- 2016-scala/src/main/scala/Day7.scala
+++ 2016-scala/src/main/scala/Day7.scala
@@ -2,6 +2,12 @@
 object Day7 {
 
   def isAbba(input: String): Boolean = {
+    input.sliding(4)
+      .exists(abbaCheck)
+  }
+
+  private def abbaCheck(input: String): Boolean = {
+    assert(input.length == 4)
     val (front, end) = input.splitAt(2)
     front == end.reverse && front != end
   }
--- 2016-scala/src/test/scala/Day7Spec.scala
+++ 2016-scala/src/test/scala/Day7Spec.scala
@@ -3,13 +3,20 @@ import org.scalatest.{Matchers, FreeSpec}
 class Day7Spec extends FreeSpec with Matchers {
 
   "Day 7" - {
-    "determines simple 4-character ABBA cases" in {
-      Day7.isAbba("abba") shouldBe true
-      Day7.isAbba("abcd") shouldBe false
-      Day7.isAbba("abcc") shouldBe false
-    }
-    "returns false if letters just repeated" in {
-      Day7.isAbba("aaaa") shouldBe false
+    "checks for abba" - {
+      "in simple 4-character cases" in {
+        Day7.isAbba("abba") shouldBe true
+        Day7.isAbba("abcd") shouldBe false
+        Day7.isAbba("abcc") shouldBe false
+      }
+      "where letters just repeated" in {
+        Day7.isAbba("aaaa") shouldBe false
+      }
+      "in longer sequences" in {
+        Day7.isAbba("abbaasdjlfk") shouldBe true
+        Day7.isAbba("asklfdabba") shouldBe true
+        Day7.isAbba("askldjfabbaaljsfk") shouldBe true
+      }
     }
   }
 }
