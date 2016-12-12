e786a02a7fc0f01d17ef210607a557860af2b909
Matt Chowning <mchowning@gmail.com>
2016-12-12 07:54:03 -0500

    clean up turnOn method and add rect method

--- 2016-scala/src/main/scala/Day8.scala
+++ 2016-scala/src/main/scala/Day8.scala
@@ -1,10 +1,15 @@
 
-class Day8(matrix: List[List[Boolean]]) {
+object Day8 {
 
   type Matrix = List[List[Boolean]]
 
-  def turnOn(x: Int, y: Int): Matrix = {
-    val newColumnX = matrix(x).updated(y, true)
-    matrix.updated(x, newColumnX)
+  def turnOn(m: Matrix)(x: Int, y: Int): Matrix = {
+    val newColumnX = m(x).updated(y, true)
+    m.updated(x, newColumnX)
+  }
+
+  def rect(m: Matrix)(wide: Int, tall: Int): Matrix = {
+    val coords = for (x <- 0 until tall; y <- 0 until wide) yield (x,y)
+    (m /: coords) { (o, cs) => turnOn(o)(cs._1, cs._2) }
   }
 }
--- 2016-scala/src/test/scala/Day8Spec.scala
+++ 2016-scala/src/test/scala/Day8Spec.scala
@@ -5,19 +5,39 @@ class Day8Spec extends FreeSpec with Matchers {
   "with an empty starting display" - {
     val column = List.fill(3)(false)
     val input: List[List[Boolean]] = List.fill(3)(column)
-    val subject = new Day8(input)
     "can turn on" - {
+      val withMatrix = Day8.turnOn(input) _
       "the top left pixel" in {
-        val result = subject.turnOn(0, 0)
-        result shouldBe List(List(true, false, false),
-                             List.fill(3)(false),
-                             List.fill(3)(false))
+        withMatrix(0, 0) shouldBe List(List(true, false, false),
+                                       List.fill(3)(false),
+                                       List.fill(3)(false))
+      }
+      "the bottom left pixel" in {
+        withMatrix(2,0) shouldBe List(List.fill(3)(false),
+                                      List.fill(3)(false),
+                                      List(true, false, false))
       }
       "the bottom right pixel" in {
-        val result = subject.turnOn(2, 2)
-        result shouldBe List(List.fill(3)(false),
-                             List.fill(3)(false),
-                             List(false, false, true))
+        withMatrix(2, 2) shouldBe List(List.fill(3)(false),
+                                       List.fill(3)(false),
+                                       List(false, false, true))
+      }
+      "the top right pixel" in {
+        withMatrix(0,2) shouldBe List(List(false, false, true),
+                                      List.fill(3)(false),
+                                      List.fill(3)(false))
+      }
+    }
+    "can handle rect command" - {
+      "that is tall" in {
+        Day8.rect(input)(1, 2) shouldBe List(List(true, false, false),
+                                             List(true, false, false),
+                                             List.fill(3)(false))
+      }
+      "that is wide" in {
+        Day8.rect(input)(2, 1) shouldBe List(List(true, true, false),
+                                             List.fill(3)(false),
+                                             List.fill(3)(false))
       }
     }
   }
