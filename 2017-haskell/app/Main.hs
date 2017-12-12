9235878ccd7d66c5c17164f0e043020e64d993c4
Matt Chowning <mchowning@gmail.com>
2017-12-12 05:40:04 -0500

    use megaparsec for day 11
    Turtle.Pattern was running really slow

--- 2017-haskell/app/Main.hs
+++ 2017-haskell/app/Main.hs
@@ -45,10 +45,10 @@ exercises = Map.fromList [ (1, (return Day1.part1, return Day1.part2))
                          , (3, (return (show Day3.part1), return (show Day3.part2)))
                          , (4, (show <$> Day4.part1, show <$> Day4.part2))
                          , (5, (show <$> Day5.part1, show <$> Day5.part2)) -- slow!
-                         , (6, (show <$> Day6.part1, show <$> Day6.part2)) -- slow!
-                         , (7, (show <$> Day7.part1, show <$> Day7.part2)) -- slow!
-                         , (8, (show <$> Day8.part1, show <$> Day8.part2)) -- slow!
-                         , (9, (show <$> Day9.part1, show <$> Day9.part2)) -- slow!
-                         , (10, (show <$> Day10.part1, show <$> Day10.part2)) -- slow!
-                         , (11, (show <$> Day11.part1, show <$> Day11.part2)) -- slow!
+                         , (6, (show <$> Day6.part1, show <$> Day6.part2))
+                         , (7, (show <$> Day7.part1, show <$> Day7.part2))
+                         , (8, (show <$> Day8.part1, show <$> Day8.part2))
+                         , (9, (show <$> Day9.part1, show <$> Day9.part2))
+                         , (10, (show <$> Day10.part1, show <$> Day10.part2))
+                         , (11, (show <$> Day11.part1, show <$> Day11.part2))
                          ]
--- 2017-haskell/src/Day11.hs
+++ 2017-haskell/src/Day11.hs
@@ -3,9 +3,12 @@
 
 module Day11 where
 
-import           Data.List        (foldl', maximum)
-import qualified Data.Text.IO     as TIO
-import           Turtle.Pattern   (Pattern, choice, match, newline, sepBy)
+import Data.Functor (($>))
+import           Data.List            (foldl', maximum)
+import           Data.Void            (Void)
+import           Text.Megaparsec      (ParseError, Parsec, choice, parse,
+                                       parseErrorPretty, sepBy1)
+import           Text.Megaparsec.Char (char, string)
 
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -30,67 +33,69 @@ newtype HexCoord =
   deriving (Show, Eq)
 
 move :: HexCoord -> Direction -> HexCoord
-move (HC (x, y, z)) =
-  \case
-    N  -> HC (x, y + 1, z - 1)
-    NE -> HC (x + 1, y, z - 1)
-    SE -> HC (x + 1, y - 1, z)
-    S  -> HC (x, y - 1, z + 1)
-    SW -> HC (x - 1, y, z + 1)
-    NW -> HC (x - 1, y + 1, z)
-
-input :: IO [Direction]
-input = head . match directionPattern <$> TIO.readFile "src/input_day11.txt"
+move (HC (x, y, z)) = \case
+  N  -> HC (x, y + 1, z - 1)
+  NE -> HC (x + 1, y, z - 1)
+  SE -> HC (x + 1, y - 1, z)
+  S  -> HC (x, y - 1, z + 1)
+  SW -> HC (x - 1, y, z + 1)
+  NW -> HC (x - 1, y + 1, z)
 
 part1Algo :: [Direction] -> Integer
 part1Algo = distanceFromRoot . foldl' move (HC (0, 0, 0))
 
 part2Algo :: [Direction] -> Integer
-part2Algo = snd . foldl' helper (HC (0,0,0),0)
-  where
-    helper (hc, maxD) dir =
-      let newHc = move hc dir
-          newMax = max maxD (distanceFromRoot newHc)
-      in (newHc, newMax)
+part2Algo = snd . foldl' helper (HC (0, 0, 0), 0)
+ where
+  helper (hc, maxD) dir =
+    let newHc  = move hc dir
+        newMax = max maxD (distanceFromRoot newHc)
+    in  (newHc, newMax)
 
-directionPattern :: Pattern [Direction]
-directionPattern = do
-  d <- choice ["n", "ne", "e", "se", "s", "sw", "w", "nw"] `sepBy` "," <* newline
-  return $ map makeDirection d
-  where
-    makeDirection =
-      \case
-        "n" -> N
-        "ne" -> NE
-        "se" -> SE
-        "s" -> S
-        "sw" -> SW
-        "nw" -> NW
-        _ -> error "no other direction value is permitted"
+input :: IO [Direction]
+input = parsedDirections >>= \case
+  Left  e  -> error (parseErrorPretty e)
+  Right ds -> return ds
+ where
+  parsedDirections :: IO (Either (ParseError Char Void) [Direction])
+  parsedDirections =
+    let filename = "src/input_day11.txt"
+    in  parse directionParser filename <$> readFile filename
+
+
+  directionParser :: Parsec Void String [Direction]
+  directionParser = choice [nw, ne, n, sw, se, s] `sepBy1` char ','
+   where
+    nw = string "nw" $> NW
+    ne = string "ne" $> NE
+    n  = string "n" $> N
+    sw = string "sw" $> SW
+    se = string "se" $> SE
+    s  = string "s" $> S
 
 distanceFromRoot :: HexCoord -> Integer
 distanceFromRoot (HC (x, y, z)) = toInteger (maximum [x, y, z])
 
 ---------------------------------------------------------------------
 tests :: IO ()
-tests = defaultMain $ testGroup "Day 11 tests"
-  [ moveTests
-  , part1AlgoTests
-  ]
-  where
-    moveTests = testGroup "move"
-      [ testCase "N" $ move (HC (0, 0, 0)) N @?= HC (0, 1, -1)
-      , testCase "NE" $ move (HC (0, 0, 0)) NE @?= HC (1, 0, -1)
-      , testCase "SE" $ move (HC (0, 0, 0)) SE @?= HC (1, -1, 0)
-      , testCase "S" $ move (HC (0, 0, 0)) S @?= HC (0, -1, 1)
-      , testCase "SW" $ move (HC (0, 0, 0)) SW @?= HC (-1, 0, 1)
-      , testCase "NW" $ move (HC (0, 0, 0)) NW @?= HC (-1, 1, 0)
-      ]
-    part1AlgoTests = testGroup "part 1 algorithm"
-      [ testCase "ne,ne,ne" $ part1Algo [NE,NE,NE] @?= 3
-      , testCase "ne,ne,sw,sw" $ part1Algo [NE,NE,SW,SW] @?= 0
-      , testCase "ne,ne,s,s" $ part1Algo [NE,NE,S,S] @?= 2
-      , testCase "se,sw,se,sw,sw" $ part1Algo [SE,SW,SE,SW,SW] @?= 3
-      ]
-       
-        
+tests = defaultMain
+  $ testGroup "Day 11 tests" [moveTests, part1AlgoTests, part2Test]
+ where
+  moveTests = testGroup
+    "move"
+    [ testCase "N" $ move (HC (0, 0, 0)) N @?= HC (0, 1, -1)
+    , testCase "NE" $ move (HC (0, 0, 0)) NE @?= HC (1, 0, -1)
+    , testCase "SE" $ move (HC (0, 0, 0)) SE @?= HC (1, -1, 0)
+    , testCase "S" $ move (HC (0, 0, 0)) S @?= HC (0, -1, 1)
+    , testCase "SW" $ move (HC (0, 0, 0)) SW @?= HC (-1, 0, 1)
+    , testCase "NW" $ move (HC (0, 0, 0)) NW @?= HC (-1, 1, 0)
+    ]
+  part1AlgoTests = testGroup
+    "part 1 algorithm"
+    [ testCase "ne,ne,ne" $ part1Algo [NE, NE, NE] @?= 3
+    , testCase "ne,ne,sw,sw" $ part1Algo [NE, NE, SW, SW] @?= 0
+    , testCase "ne,ne,s,s" $ part1Algo [NE, NE, S, S] @?= 2
+    , testCase "se,sw,se,sw,sw" $ part1Algo [SE, SW, SE, SW, SW] @?= 3
+    , testCase "actual input" $ part1 >>= (@?=715)
+    ]
+  part2Test = testCase "part 2 test on actual input" $ part2 >>= (@?=1512)
