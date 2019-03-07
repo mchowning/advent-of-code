9c4bc6b06ed8a34c42be93ef1e13405457a4d040
Matt Chowning <mchowning@gmail.com>
2019-03-07 08:25:42 -0500

    Cleanup day1

--- 2018-clojure/src/advent_of_code/day1.clj
+++ 2018-clojure/src/advent_of_code/day1.clj
@@ -1,27 +1,25 @@
 (ns advent-of-code.day1)
 
-(defn funcForLine [line]
-  #(+ % (read-string line)))
+(def lines
+  (map read-string
+       (clojure.string/split-lines
+        (slurp "input/day1-1.txt"))))
 
 (defn accumulateLines [lines]
-  (reduce + (map read-string lines)))
-
-(defn process-input [filename]
-  (clojure.string/split-lines
-   (slurp
-    (str "input/" filename))))
-
-(def input (process-input "day1-1.txt"))
+  (reduce + lines))
 
 (defn process-part2-lines [lines]
-  (loop
-   [[f & fs] (cycle (map funcForLine lines))
-    seen #{0}
-    total 0]
-    (let [newTotal (f total)]
-      (if (contains? seen newTotal)
-        newTotal
-        (recur fs (conj seen newTotal) newTotal)))))
+  (first-duplicate (reductions + (cycle lines))))
+
+(defn first-duplicate [totals]
+  (reduce #(if (%1 %2) (reduced %2) (conj %1 %2))
+          #{0}
+          totals))
+
+(defn funcForLine [line]
+  #(+ % line))
+
+;; ----------------------------------------------------------
 
 (def part2 (process-part2-lines input))
 (def part1 (accumulateLines input))
