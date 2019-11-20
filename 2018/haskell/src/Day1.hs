f8c712081202e3ac755b75cf9a7a412fd300036c
Matt Chowning <mchowning@gmail.com>
2019-11-20 17:28:09 -0500

    Alternative day1 haskell implementation

--- 2018/haskell/src/Day1.hs
+++ 2018/haskell/src/Day1.hs
@@ -20,33 +20,43 @@ part1 = sum <$> parseInput
 -----------------
 
 part2 :: IO Int
-part2 = do
-  repeatingList <- cycle <$> parseInput
-  let runningTotal = scanl1 (+) repeatingList
-  return (firstDup runningTotal)
+part2 =
+  -- firstDup . scanl1 (+) . cycle <$> parseInput
+
+  do
+    input <- parseInput
+    let runningTotal = scanl1 (+) (cycle input)
+    return (firstDup runningTotal)
 
 firstDup :: [Int] -> Int
-firstDup ls = evalState (getFirstDuplicate ls) IntSet.empty
+firstDup = go IntSet.empty
+  where
+    go seen (x:xs)
+      | x `IntSet.member` seen = x
+      | otherwise = go (IntSet.insert x seen) xs
 
-getFirstDuplicate :: [Int] -> State IntSet Int
-getFirstDuplicate (n:ns) = do
-  isDuplicate <- gets (member n)
-  if isDuplicate
-    then return n
-    else do
-      modify (insert n)
-      getFirstDuplicate ns
+-- firstDup :: [Int] -> Int
+-- firstDup ls = evalState (getFirstDuplicate ls) IntSet.empty
+
+-- getFirstDuplicate :: [Int] -> State IntSet Int
+-- getFirstDuplicate (n:ns) = do
+--   isDuplicate <- gets (member n)
+--   if isDuplicate
+--     then return n
+--     else do
+--       modify (insert n)
+--       getFirstDuplicate ns
 
 -----------------
 type Parser = Parsec Void Text
 
 parseInput :: IO [Int]
-parseInput = parse (signedInt `sepBy1` eol) <$> T.readFile "../input.txt"
+parseInput = parse (signedInt `sepBy1` eol) "input.txt" <$> T.readFile "../input.txt"
   where
     signedInt = signed (return ()) decimal
 
-parse :: Parser [Int] -> Text -> [Int]
-parse parser input = processEither (runParser parser "input.txt" input)
+parse :: Parser a -> Text -> Text -> a
+parse parser filename input = processEither (runParser parser (T.unpack filename) input)
   where
     processEither (Left  e ) = error (errorBundlePretty e)
     processEither (Right rs) = rs
