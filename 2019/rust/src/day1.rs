0a84001fe1682bddf26bdefa3c0d2b98bf058649
Matt Chowning <mchowning@gmail.com>
2019-12-01 10:21:17 -0500

    Bit of cleanup

--- 2019/rust/src/day1.rs
+++ 2019/rust/src/day1.rs
@@ -1,19 +1,19 @@
 use std::fs;
 
 pub fn part1() -> i32 {
-    read_file()
-        .lines()
+    parse_lines(&read_input())
         .map(|line|
-            calculate_fuel(line.parse::<i32>().unwrap())
+            calculate_fuel(line)
         )
         .sum()
 }
 
 pub fn part2() -> i32 {
-    read_file()
-        .lines()
+
+    parse_lines(&read_input())
         .map(|line| {
-            calculate_fuel_recursive(line.parse::<i32>().unwrap())
+            calculate_fuel_recursive(line)
+//            recursive_fuel(line).sum::<i32>()
         })
         .sum()
 }
@@ -24,14 +24,31 @@ fn calculate_fuel(start: i32) -> i32 {
 
 fn calculate_fuel_recursive(start: i32) -> i32 {
     let fuel = calculate_fuel(start);
-    if fuel > 0 {
-        fuel + calculate_fuel_recursive(fuel)
-    } else {
+    if fuel <= 0 {
         0
+    } else {
+        fuel + calculate_fuel_recursive(fuel)
     }
 }
 
-fn read_file() -> String {
+//fn recursive_fuel(start: i32) -> impl Iterator<Item=i32> {
+//    std::iter::successors(
+//        Some(start),
+//        | &num | {
+//            let fuel = calculate_fuel(num);
+//            if fuel <= 0 { None } else { Some(fuel) }
+//        }
+//    ).skip(1) // ignore start value because it isn't fuel for any other fuel
+//}
+
+fn parse_lines<'a>(file: &'a str) -> impl Iterator<Item=i32> + 'a {
+    file.lines()
+        .map( |line|
+            line.parse::<i32>().unwrap()
+        )
+}
+
+fn read_input() -> String {
     fs::read_to_string("../inputs/day1.txt")
         .expect("Something went wrong reading input")
 }
--- 2019/rust/src/main.rs
+++ 2019/rust/src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
+    println!("{}", day1::part1());
     println!("{}", day1::part2());
 }
