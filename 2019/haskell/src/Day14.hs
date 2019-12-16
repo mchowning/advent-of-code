910fb48ae9623ef96b654107a960a863fbdb86a0
Matt Chowning <mchowning@gmail.com>
2019-12-15 21:30:36 -0500

    Day 14 initial implementation

--- 2019/haskell/src/Day14.hs
+++ 2019/haskell/src/Day14.hs
@@ -1,252 +1,117 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
 
 module Day14 where
 
 import Util
 
---import Data.Text (Text)
-import Data.Functor (($>))
-import Data.Maybe (catMaybes, fromMaybe)
+import Control.Monad (void)
 import Data.List (foldl')
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
-import Control.Monad (guard, void)
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import           Text.Megaparsec.Char.Lexer
 
 import Debug.Trace
 
-data Ingredient = Ingredient
-                { name :: String
-                , quantity :: Integer
-                } deriving Show
+type ChemicalAmount = (String, Int)
 
-newtype IngredientCollection = IngredientCollection { toMap :: M.Map String Integer }
-                             deriving (Eq, Monoid, Ord, Semigroup, Show)
+type Reaction = ([ChemicalAmount], ChemicalAmount)
 
-ingredients :: IngredientCollection -> [Ingredient]
-ingredients (IngredientCollection m) = uncurry Ingredient <$> M.toList m
+type ReactionMap = M.Map String Reaction
 
-data Reaction = Reaction { inputs :: IngredientCollection
-                         , output :: Ingredient
-                         } deriving Show
+type Inventory = M.Map String Int
 
-mkIngredientCollection :: [Ingredient] -> IngredientCollection
-mkIngredientCollection = foldl' (flip addIngredient) mempty
-
-removeIngredient :: Ingredient -> IngredientCollection -> Maybe IngredientCollection
-removeIngredient Ingredient {..} (IngredientCollection allAvailable) =
-    let newValue = fromMaybe 0 (allAvailable M.!? name) - quantity
-    in if | newValue > 0 -> Just . IngredientCollection $ M.insert name newValue allAvailable
-          | newValue == 0 -> Just . IngredientCollection $ M.delete name allAvailable
-          | newValue < 0 -> Nothing
-
-
-addIngredient :: Ingredient -> IngredientCollection -> IngredientCollection
-addIngredient Ingredient {..} (IngredientCollection coll) =
-  IngredientCollection (M.insertWith (+) name quantity coll)
-
-reverseReaction :: Reaction  -> IngredientCollection -> Maybe IngredientCollection
-reverseReaction Reaction {..} ic =
-  let withoutOutput = removeIngredient output ic
-      withInput = foldr (fmap . addIngredient) withoutOutput (ingredients inputs)
-  in withInput
-
-performReaction :: Reaction -> IngredientCollection -> Maybe IngredientCollection
-performReaction Reaction {..} ic =
-  let withoutInputs = foldr (\i mic -> removeIngredient i =<< mic) (Just ic) (ingredients inputs)
-      withOuptut = addIngredient output <$> withoutInputs
-  in withOuptut
-
-amountRequired :: String -> [Reaction] -> IngredientCollection -> [IngredientCollection]
-amountRequired what rs (IngredientCollection m)
-  | null m = [mempty]
-  | otherwise =
-    let amountOfWhat = fromMaybe 0 (M.lookup what m)
-        rest = M.delete what m
-        reducedRest = catMaybes (flip reverseReaction (IngredientCollection rest) <$> rs)
-        result = if | null rest ->
-                        [IngredientCollection m] -- SUCCESS! We've reduced down to just what we're seeking
-                    | null reducedRest ->
-                        [] -- FAILURE! We have other stuff, but it can't be reduced, so fail
-                    | otherwise ->
-                        addIngredient (Ingredient what amountOfWhat) <$> concatMap (amountRequired what rs) reducedRest
-    in result
-
-consumeAndRun1 :: [Reaction] -- available reactions
-              -> IngredientCollection -- available ingredients
-              -> [Ingredient] -- lazy stream of ingredients to consume
-              -> (IngredientCollection -> Bool) -- predicate to stop consumption
-              -> S.Set IngredientCollection -- already processed
-              -> [(IngredientCollection, IngredientCollection)] -- consumed ingredients and resulting output
-consumeAndRun1 rs available (i:is) pred s =
-  let (resultsFromThisInput, s') = consumeAndRun' rs [(available, available)] pred s
-      newInput = addIngredient i available
---  in trace (show . S.size $ s') $ resultsFromThisInput ++ consumeAndRun1 rs newInput is pred s'
-  in resultsFromThisInput ++ consumeAndRun1 rs newInput is pred s
-
-consumeAndRun' :: [Reaction] -- available reactions
-              -> [(IngredientCollection, IngredientCollection)] -- consumed and resulting ingredients to process
-              -> (IngredientCollection -> Bool) -- predicate to stop consumption
-              -> S.Set IngredientCollection -- collections already queued
-              -> ([(IngredientCollection, IngredientCollection)], S.Set IngredientCollection) -- consumed ingredients and resulting output
-consumeAndRun' rs [] _ s = ([], s)
-consumeAndRun' rs ((consumed, resulting):crs) pred s
-  | S.member resulting s = ([], s)
-  | pred resulting = ([(consumed, resulting)], (S.insert resulting s))
-  | otherwise =
-    let afterReactions = catMaybes (flip performReaction resulting <$> rs)
-        (plainResults, s') = consumeAndRun' rs crs pred (S.insert resulting s)
-        (afterResults, s'') = consumeAndRun' rs ((consumed, ) <$> afterReactions) pred s'
-    in (plainResults ++ afterResults, s'')
-
-infiniteOre :: [Ingredient]
-infiniteOre = Ingredient "ORE" 1 : infiniteOre
-
-infiniteOre' :: Int -> [Ingredient]
-infiniteOre' n = Ingredient "ORE" 1 : infiniteOre' (n+1)
---infiniteOre' n = trace (show n) $ Ingredient "ORE" 1 : infiniteOre' (n+1)
-
-------------------------------------------------------------------------------------------
-
-fillRequirement :: S.Set IngredientCollection -> [Reaction] -> IngredientCollection -> (IngredientCollection -> Bool) -> [IngredientCollection]
-fillRequirement s rs i@(IngredientCollection m) pred
-  | pred i = [i]
-  | S.member i s = []
-  | otherwise =
-      let remainingRequirements = M.filter (> 0) m
-          matchingReactions = filter ((`M.member` remainingRequirements) . name . output) rs
-      in if null matchingReactions
-           then trace ("could not find any matching reactions with " <> show m) []
-           else let newRequirements = flip reverseReaction' i <$> matchingReactions
-                    filteredRequirements = filter (not . flip S.member s) newRequirements
-                in concatMap (\newReq -> fillRequirement (S.insert i s) rs newReq pred) filteredRequirements
-
-reverseReaction' :: Reaction
-                 -> IngredientCollection -- requirements
-                 -> IngredientCollection -- remaining requirements after reaction
-reverseReaction' Reaction {..} ic =
-  let withoutOutput = removeIngredient' output ic
-      withInput = foldr addIngredient withoutOutput (ingredients inputs)
-  in withInput
-
-
-removeIngredient' :: Ingredient -> IngredientCollection -> IngredientCollection
-removeIngredient' Ingredient {..} (IngredientCollection allAvailable) =
-    let newValue = fromMaybe 0 (allAvailable M.!? name) - quantity
-    -- FIXME when we buy extra we need to save that amount, BUT NOT AS A REQUIREMENT, it is an asset
-    in IngredientCollection $ M.insert name newValue allAvailable
-
-------------------------------------------------------------------------------------------
-
--- 431448
-part1 :: IO Integer
+part1 :: IO Int
 part1 = part1' <$> input
 
-collectionToMap :: IngredientCollection -> M.Map String Integer
-collectionToMap (IngredientCollection m) = m
+part1' :: ReactionMap -> Int
+part1' rm = oreFor rm ("FUEL", 1)
 
-part1' :: [Reaction] -> Integer
-part1' = oreForFuel 1
+oreFor :: ReactionMap -> ChemicalAmount -> Int
+oreFor rm desired =
+  case produce rm mempty desired M.!? "ORE" of
+    Just n -> n
+    Nothing -> error "produce resulted in no ore"
 
-oreForFuel :: Integer -> [Reaction] -> Integer
-oreForFuel n rs =
-  let remainingIngredients (IngredientCollection m) = M.filter (> 0) m
-      onlyOreRemains = (== ["ORE"]) . M.keys . remainingIngredients
-      candidates = fillRequirement S.empty rs (mkIngredientCollection [Ingredient "FUEL" n]) onlyOreRemains
-      requiredOre = head (remainingIngredients <$> candidates)
-  in head (remainingIngredients <$> candidates) M.! "ORE"
+
+produce :: ReactionMap -- possible reactions
+        -> Inventory -- available inventory
+        -> ChemicalAmount -- desired
+        -> Inventory -- inventory updated to have desired
+produce _ inventory ("ORE", n) = M.insertWith (+) "ORE" n inventory
+produce reactionMap inventory (desiredName, desiredAmount) =
+  let available = M.findWithDefault 0 desiredName inventory
+  in if available >= desiredAmount
+       then M.insert desiredName (available - desiredAmount) inventory
+       else
+         let additionalNeeded = desiredAmount - available
+             reactionToGetMore = case reactionMap M.!? desiredName of
+                                   Just r -> r
+                                   Nothing -> error ("Could not find reaction that creates " <> desiredName)
+             (newChems, (_, newAmount)) = minimumIngredients reactionToGetMore additionalNeeded
+             newInventory = M.insert desiredName (newAmount - additionalNeeded) inventory
+         in foldl' (produce reactionMap) newInventory newChems
+
+minimumIngredients :: Reaction -- reaction to use
+                   -> Int  -- minmimum desired output quantity
+                   -> ([ChemicalAmount], ChemicalAmount) -- reaction multiplied by number of reactions needed
+minimumIngredients (rInputs, output@(outputName, outputNum)) desiredAmount =
+  let numReactionsNeeded = (1 + ((desiredAmount - 1) `div` outputNum))
+      ingredients = (fmap . fmap) (* numReactionsNeeded) rInputs
+      resultingOutput = fmap (* numReactionsNeeded) output
+  in (ingredients, resultingOutput)
+
 
 ------------------------------------------------------------------------------------------
 
-fillRequirement' :: S.Set IngredientCollection -> [Reaction] -> (IngredientCollection, [Reaction]) -> (IngredientCollection -> Bool) -> [(IngredientCollection, [Reaction])]
-fillRequirement' s rs (i@(IngredientCollection m), accRs) pred
-  | pred i = [(i,accRs)]
-  | S.member i s = []
+part2 :: IO Int
+part2 = part2' <$> input
+
+part2' :: ReactionMap -> Int
+part2' = fuelFromOre 1000000000000
+
+fuelFromOre :: Int -> ReactionMap -> Int
+fuelFromOre numOre rm = binarySearchBiggestWhich 0 numOre (\i -> oreFor rm ("FUEL", i) < numOre)
+
+binarySearchBiggestWhich :: Int
+                         -> Int
+                         -> (Int -> Bool)
+                         -> Int
+binarySearchBiggestWhich lower upper pred
+  | lower == upper = if pred lower 
+                       then lower 
+                       else error "search localized to value that failed predicate"
   | otherwise =
-      let remainingRequirements = M.filter (> 0) m
-          matchingReactions = filter ((`M.member` remainingRequirements) . name . output) rs
-      in if null matchingReactions
-           then trace ("could not find any matching reactions with " <> show m) []
-           else let newRequirements = (\r -> (reverseReaction' r i, r: accRs)) <$> matchingReactions
-                    filteredRequirements = filter (not . flip S.member s . fst) newRequirements
-                    newSet = S.insert i s
-                in concatMap (\newReq -> fillRequirement' newSet rs newReq pred) filteredRequirements
+      let mid = (lower + upper + 1) `div` 2 -- add 1 to make sure it rounds toward upper
+      in if pred mid
+           then binarySearchBiggestWhich mid upper pred
+           else binarySearchBiggestWhich lower (mid - 1) pred
 
---reverseReaction' :: Reaction
---                 -> IngredientCollection -- requirements
---                 -> IngredientCollection -- remaining requirements after reaction
---reverseReaction' Reaction {..} ic =
---  let withoutOutput = removeIngredient' output ic
---      withInput = foldr addIngredient withoutOutput (ingredients inputs)
---  in withInput
---
---
---removeIngredient' :: Ingredient -> IngredientCollection -> IngredientCollection
---removeIngredient' Ingredient {..} (IngredientCollection allAvailable) =
---    let newValue = fromMaybe 0 (allAvailable M.!? name) - quantity
---    -- FIXME when we buy extra we need to save that amount, BUT NOT AS A REQUIREMENT, it is an asset
---    in IngredientCollection $ M.insert name newValue allAvailable
-
-part2' :: Int -> [Reaction] -> IngredientCollection
-part2' n rs = part2'' n rs (mkIngredientCollection [Ingredient "ORE" 1000000000000])
---  let remainingIngredients (IngredientCollection m) = M.filter (> 0) m
---      onlyOreRemains = (== ["ORE"]) . M.keys . remainingIngredients
---      candidates = fillRequirement' S.empty rs (mkIngredientCollection [Ingredient "FUEL" n], []) onlyOreRemains
-----      requiredOre = head (remainingIngredients <$> candidates)
-----  in head (remainingIngredients <$> candidates) M.! "ORE"
---      (is, actualRs) = head candidates
---      amountOfOre = toMap is M.! "ORE"
-----  in trace (show actualRs) $ runAll (mkIngredientCollection [Ingredient "ORE" 1000000000000]) actualRs
---   in iterate (`runAll` actualRs) (mkIngredientCollection [Ingredient "ORE" 1000000000000]) !! 10000
-
--- iterate (`runAll` reactionsToFuel rs) (mkIngredientCollection [Ingredient "ORE" 1000000000000]) !! n
-
-part2'' :: Int -> [Reaction] -> IngredientCollection -> IngredientCollection
-part2'' n rs ic =
- iterate (`runAll` reactionsToFuel rs) ic !! n
+------------------------------------------------------------------------------------------
 
 
-reactionsToFuel :: [Reaction] -> [Reaction]
-reactionsToFuel rs =
-  let remainingIngredients (IngredientCollection m) = M.filter (> 0) m
-      onlyOreRemains = (== ["ORE"]) . M.keys . remainingIngredients
-      candidates = fillRequirement' S.empty rs (mkIngredientCollection [Ingredient "FUEL" 1], []) onlyOreRemains
---      requiredOre = head (remainingIngredients <$> candidates)
---  in head (remainingIngredients <$> candidates) M.! "ORE"
-      (_, actualRs) = head candidates
-   in actualRs
-
-
-runAll :: IngredientCollection -> [Reaction] -> IngredientCollection
-runAll = foldl' (\ic r -> fromMaybe ic (performReaction r ic))
-
-
-
-input :: IO [Reaction]
+input :: IO ReactionMap
 input = parseInput parser "day14.txt"
 
-parser :: Parser [Reaction]
-parser = reactionParser `sepBy1` eol
+mkReactionMap :: [Reaction] -> ReactionMap
+mkReactionMap rs = M.fromList [ (outputName, reaction) | reaction <- rs, let outputName = fst (snd reaction)]
+
+parser :: Parser ReactionMap
+parser = mkReactionMap <$> reactionParser `sepBy1` eol
 
 reactionParser :: Parser Reaction
 reactionParser = do
   ingredients <- ingredientParser `sepBy1` string ", "
   void (string " => ")
   output <- ingredientParser
-  return $ Reaction (mkIngredientCollection ingredients) output
+  return (ingredients, output)
 
-ingredientParser :: Parser Ingredient
+ingredientParser :: Parser ChemicalAmount
 ingredientParser = do
   num <- decimal
   space1
   name <- some letterChar
-  return (Ingredient name num)
+  return (name, num)
 
--- 2019/haskell/test/Day14Tests.hs
+++ 2019/haskell/test/Day14Tests.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Day14Tests where
 
@@ -11,8 +12,17 @@ import Data.Text (Text)
 import Test.Tasty
 import Test.Tasty.HUnit
 
+day14Tests = testGroup "day 14"
+  [ testCase "part 1" do
+      result <- part1
+      result @?= 431448
+  , testCase "part 2" do
+      result <- part2
+      result @?= 3279311
+  ]
+
 example1_oreFor1Fuel = 31
-example1 :: [Reaction]
+example1 :: ReactionMap
 example1 = parse' parser "example1" "10 ORE => 10 A\n\
                                     \1 ORE => 1 B\n\
                                     \7 A, 1 B => 1 C\n\
@@ -21,7 +31,7 @@ example1 = parse' parser "example1" "10 ORE => 10 A\n\
                                     \7 A, 1 E => 1 FUEL"
 
 example2_oreFor1Fuel = 165
-example2 :: [Reaction]
+example2 :: ReactionMap
 example2 = parse' parser "example2" "9 ORE => 2 A\n\
                                     \8 ORE => 3 B\n\
                                     \7 ORE => 5 C\n\
@@ -32,7 +42,7 @@ example2 = parse' parser "example2" "9 ORE => 2 A\n\
 
 example3_oreFor1Fuel = 13312
 example3_fuelForTrillionOre = 82892753
-example3 :: [Reaction]
+example3 :: ReactionMap
 example3 = parse' parser "example3" "157 ORE => 5 NZVS\n\
                                     \165 ORE => 6 DCFZ\n\
                                     \44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n\
@@ -45,7 +55,7 @@ example3 = parse' parser "example3" "157 ORE => 5 NZVS\n\
 
 example4_oreFor1Fuel = 180697
 example4_fuelForTrillionOre = 5586022
-example4 :: [Reaction]
+example4 :: ReactionMap
 example4 = parse' parser "example4" "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n\
                                     \17 NVRVD, 3 JNWZP => 8 VPVL\n\
                                     \53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n\
@@ -61,7 +71,7 @@ example4 = parse' parser "example4" "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STK
 
 example5_oreFor1Fuel = 2210736
 example5_fuelForTrillionOre = 460664
-example5 :: [Reaction]
+example5 :: ReactionMap
 example5 = parse' parser "example5" "171 ORE => 8 CNZTR\n\
                                     \7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n\
                                     \114 ORE => 4 BHXH\n\
@@ -79,60 +89,3 @@ example5 = parse' parser "example5" "171 ORE => 8 CNZTR\n\
                                     \121 ORE => 7 VRPVC\n\
                                     \7 XCVML => 6 RJRHP\n\
                                     \5 BHXH, 4 VRPVC => 5 LTCX"
-
-simpleCollection = IngredientCollection (M.singleton "a" 1)
-
-day14Tests = testGroup "day 14"
-  [ testCase "part 1" $ part1 >>= (@?= 431448)
-  , testGroup "remove ingredient"
-    [ testCase "subtracts" $
-        removeIngredient (Ingredient "a" 1) (IngredientCollection (M.singleton "a" 1)) @?= Just mempty
-    , testCase "won't go below 0" $
-        removeIngredient (Ingredient "a" 2) (mkIngredientCollection [Ingredient "a" 1]) @?= Nothing
-    ]
-  , testGroup "add ingredient"
-    [ testCase "add 1 to empty collection gives 1" $
-        addIngredient (Ingredient "a" 1) mempty @?= mkIngredientCollection [Ingredient "a" 1]
-    , testCase "add 1 to collection with 3 already gives 4" $
-        addIngredient (Ingredient "a" 1) (mkIngredientCollection [Ingredient "a" 3]) @?= mkIngredientCollection [Ingredient "a" 4]
-    ]
-  , testGroup "reactions"
-    [ testCase "reverse" $
-        let reaction = Reaction (mkIngredientCollection [Ingredient "a" 2, Ingredient "b" 10]) (Ingredient "c" 2)
-        in reverseReaction reaction (mkIngredientCollection [Ingredient "c" 3]) @?= Just (mkIngredientCollection [Ingredient "a" 2, Ingredient "b" 10, Ingredient "c" 1])
-    , testCase "forward" $
-        let reaction = Reaction (mkIngredientCollection [Ingredient "a" 2, Ingredient "b" 10]) (Ingredient "c" 2)
-            start = mkIngredientCollection [Ingredient "a" 2, Ingredient "b" 11, Ingredient "c" 10]
-        in performReaction reaction start @?= Just (mkIngredientCollection [Ingredient "b" 1, Ingredient "c" 12])
-
-    , testCase "reverse' with extra" $
-        let reaction = Reaction (mkIngredientCollection [Ingredient "a" 1]) (Ingredient "c" 2)
-        in reverseReaction' reaction (mkIngredientCollection [Ingredient "c" 1]) @?= mkIngredientCollection [Ingredient "a" 1, Ingredient "c" (-1)]
-    , testCase "reverse' with 2 inputesextra" $
-        let reaction = Reaction (mkIngredientCollection [Ingredient "a" 1, Ingredient "b" 2]) (Ingredient "c" 2)
-        in reverseReaction' reaction (mkIngredientCollection [Ingredient "c" 1]) @?= mkIngredientCollection [Ingredient "a" 1, Ingredient "b" 2, Ingredient "c" (-1)]
-    ]
-  ]
-
-mkIngredient :: (String, Integer) -> Ingredient
-mkIngredient (s,i) = Ingredient s i
-
--- too low: 2320000
-
--- 998
-part2_1000 = mkIngredientCollection . fmap mkIngredient $ [("BCNV",2000),("BJVQM",2036),("BLPJK",2000),("BMJR",1014),("CTVK",1009),("CWBNX",34),("DGFG",4010),("DZFNW",5994),("FDSX",29),("FHFH",2005),("FJRFG",5014),("FUEL",998),("GSCG",2002),("HPKB",1020),("JFKGV",7022),("JHDW",4),("JHXBM",8),("JLRM",7008),("JNKH",5),("JQNF",5098),("KDFNZ",12),("KGSDJ",5004),("KQDVM",21),("KZFPJ",6000),("LKPQG",4),("LMNCH",4002),("MGCQ",4001),("MPKR",2002),("MQXF",2002),("MZKG",4022),("NPRST",7025),("NWDZ",1060),("ORE",999568552000),("PQZXH",2007),("PVTL",3040),("QMBM",48),("QVNKN",3005),("QWKRZ",15),("RBXB",20),("RFLX",4),("RPWKC",4012),("SXKT",1074),("VJMWM",9),("VJNBT",2002),("VLWQB",10),("VQDT",3013),("VRQR",1036),("WBQR",2),("WHVXC",5003),("WJDF",5008),("XLFZJ",5998),("XQBS",2),("XSNKB",3025),("ZJGH",3034),("ZKGMX",2006),("ZKTX",1037),("ZSCX",3000)]
-
--- 1998
-part2_2000 = mkIngredientCollection . fmap mkIngredient $ [("BCNV",4000),("BJVQM",4036),("BLPJK",4000),("BMJR",2014),("CTVK",2009),("CWBNX",34),("DGFG",8010),("DZFNW",11994),("FDSX",29),("FHFH",4005),("FJRFG",10014),("FUEL",1998),("GSCG",4002),("HPKB",2020),("JFKGV",14022),("JHDW",4),("JHXBM",8),("JLRM",14008),("JNKH",5),("JQNF",10098),("KDFNZ",12),("KGSDJ",10004),("KQDVM",21),("KZFPJ",12000),("LKPQG",4),("LMNCH",8002),("MGCQ",8001),("MPKR",4002),("MQXF",4002),("MZKG",8022),("NPRST",14025),("NWDZ",2060),("ORE",999137104000),("PQZXH",4007),("PVTL",6040),("QMBM",48),("QVNKN",6005),("QWKRZ",15),("RBXB",20),("RFLX",4),("RPWKC",8012),("SXKT",2074),("VJMWM",9),("VJNBT",4002),("VLWQB",10),("VQDT",6013),("VRQR",2036),("WBQR",2),("WHVXC",10003),("WJDF",10008),("XLFZJ",11998),("XQBS",2),("XSNKB",6025),("ZJGH",6034),("ZKGMX",4006),("ZKTX",2037),("ZSCX",6000)]
-
--- 2998
-part2_3000 = mkIngredientCollection . fmap mkIngredient $ [("BCNV",6000),("BJVQM",6036),("BLPJK",6000),("BMJR",3014),("CTVK",3009),("CWBNX",34),("DGFG",12010),("DZFNW",17994),("FDSX",29),("FHFH",6005),("FJRFG",15014),("FUEL",2998),("GSCG",6002),("HPKB",3020),("JFKGV",21022),("JHDW",4),("JHXBM",8),("JLRM",21008),("JNKH",5),("JQNF",15098),("KDFNZ",12),("KGSDJ",15004),("KQDVM",21),("KZFPJ",18000),("LKPQG",4),("LMNCH",12002),("MGCQ",12001),("MPKR",6002),("MQXF",6002),("MZKG",12022),("NPRST",21025),("NWDZ",3060),("ORE",998705656000),("PQZXH",6007),("PVTL",9040),("QMBM",48),("QVNKN",9005),("QWKRZ",15),("RBXB",20),("RFLX",4),("RPWKC",12012),("SXKT",3074),("VJMWM",9),("VJNBT",6002),("VLWQB",10),("VQDT",9013),("VRQR",3036),("WBQR",2),("WHVXC",15003),("WJDF",15008),("XLFZJ",17998),("XQBS",2),("XSNKB",9025),("ZJGH",9034),("ZKGMX",6006),("ZKTX",3037),("ZSCX",9000)]
-
--- 3998
-part2_4000 = mkIngredientCollection . fmap mkIngredient $ [("BCNV",8000),("BJVQM",8036),("BLPJK",8000),("BMJR",4014),("CTVK",4009),("CWBNX",34),("DGFG",16010),("DZFNW",23994),("FDSX",29),("FHFH",8005),("FJRFG",20014),("FUEL",3998),("GSCG",8002),("HPKB",4020),("JFKGV",28022),("JHDW",4),("JHXBM",8),("JLRM",28008),("JNKH",5),("JQNF",20098),("KDFNZ",12),("KGSDJ",20004),("KQDVM",21),("KZFPJ",24000),("LKPQG",4),("LMNCH",16002),("MGCQ",16001),("MPKR",8002),("MQXF",8002),("MZKG",16022),("NPRST",28025),("NWDZ",4060),("ORE",998274208000),("PQZXH",8007),("PVTL",12040),("QMBM",48),("QVNKN",12005),("QWKRZ",15),("RBXB",20),("RFLX",4),("RPWKC",16012),("SXKT",4074),("VJMWM",9),("VJNBT",8002),("VLWQB",10),("VQDT",12013),("VRQR",4036),("WBQR",2),("WHVXC",20003),("WJDF",20008),("XLFZJ",23998),("XQBS",2),("XSNKB",12025),("ZJGH",12034),("ZKGMX",8006),("ZKTX",4037),("ZSCX",12000)]
-
--- 4998
-part2_5000 = mkIngredientCollection . fmap mkIngredient $ [("BCNV",10000),("BJVQM",10036),("BLPJK",10000),("BMJR",5014),("CTVK",5009),("CWBNX",34),("DGFG",20010),("DZFNW",29994),("FDSX",29),("FHFH",10005),("FJRFG",25014),("FUEL",4998),("GSCG",10002),("HPKB",5020),("JFKGV",35022),("JHDW",4),("JHXBM",8),("JLRM",35008),("JNKH",5),("JQNF",25098),("KDFNZ",12),("KGSDJ",25004),("KQDVM",21),("KZFPJ",30000),("LKPQG",4),("LMNCH",20002),("MGCQ",20001),("MPKR",10002),("MQXF",10002),("MZKG",20022),("NPRST",35025),("NWDZ",5060),("ORE",997842760000),("PQZXH",10007),("PVTL",15040),("QMBM",48),("QVNKN",15005),("QWKRZ",15),("RBXB",20),("RFLX",4),("RPWKC",20012),("SXKT",5074),("VJMWM",9),("VJNBT",10002),("VLWQB",10),("VQDT",15013),("VRQR",5036),("WBQR",2),("WHVXC",25003),("WJDF",25008),("XLFZJ",29998),("XQBS",2),("XSNKB",15025),("ZJGH",15034),("ZKGMX",10006),("ZKTX",5037),("ZSCX",15000)]
-
--- 9998
-part2_10000 = mkIngredientCollection . fmap mkIngredient $ [("BCNV",20000),("BJVQM",20036),("BLPJK",20000),("BMJR",10014),("CTVK",10009),("CWBNX",34),("DGFG",40010),("DZFNW",59994),("FDSX",29),("FHFH",20005),("FJRFG",50014),("FUEL",9998),("GSCG",20002),("HPKB",10020),("JFKGV",70022),("JHDW",4),("JHXBM",8),("JLRM",70008),("JNKH",5),("JQNF",50098),("KDFNZ",12),("KGSDJ",50004),("KQDVM",21),("KZFPJ",60000),("LKPQG",4),("LMNCH",40002),("MGCQ",40001),("MPKR",20002),("MQXF",20002),("MZKG",40022),("NPRST",70025),("NWDZ",10060),("ORE",995685520000),("PQZXH",20007),("PVTL",30040),("QMBM",48),("QVNKN",30005),("QWKRZ",15),("RBXB",20),("RFLX",4),("RPWKC",40012),("SXKT",10074),("VJMWM",9),("VJNBT",20002),("VLWQB",10),("VQDT",30013),("VRQR",10036),("WBQR",2),("WHVXC",50003),("WJDF",50008),("XLFZJ",59998),("XQBS",2),("XSNKB",30025),("ZJGH",30034),("ZKGMX",20006),("ZKTX",10037),("ZSCX",30000)]
